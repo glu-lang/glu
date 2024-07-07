3bdb7947ccbe88369731aeae73184d00596ec1c8
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2024-07-07 12:31:12 +0200

    feat: build system compile with embedded LLVM

--- cmake/FetchLLVM.cmake
+++ cmake/FetchLLVM.cmake
@@ -1,10 +1,13 @@
 include (FetchContent)
 
-FetchContent_Declare (LLVM
+FetchContent_Declare(
+    LLVM
     GIT_REPOSITORY "https://github.com/llvm/llvm-project.git"
     GIT_TAG "llvmorg-18.1.8"
     GIT_SHALLOW 1
-    SOURCE_SUBDIR "llvm/"
+    SOURCE_SUBDIR llvm
+    OVERRIDE_FIND_PACKAGE
 )
 
+set(LLVM_BUILD_LLVM_DYLIB ON)
 FetchContent_MakeAvailable(LLVM)
