2acf6d7fba0495d97660ba4f6dafceda464443d2
Emil Pedersen <emil.pedersen@epitech.eu>
2024-10-02 03:36:26 -0400

    feat(gil): support more operands in instructions

--- include/GIL/InstBase.hpp
+++ include/GIL/InstBase.hpp
@@ -16,6 +16,100 @@ enum class InstKind {
     #undef GIL_INSTRUCTION
 };
 
+enum class OperandKind {
+    /// The operand is a value defined by an instruction, or an argument of a basic block. (%0, %1, etc.) (GILValue)
+    ValueKind,
+    /// The operand is a literal integer value. (42, -1, etc.)
+    LiteralIntKind,
+    /// The operand is a literal floating-point value. (3.14, -0.5, etc.)
+    LiteralFloatKind,
+    /// The operand is a literal string value. ("Hello, world!", etc.)
+    LiteralStringKind,
+    /// The operand is a reference to a function or global symbol. (@main, @printf, etc.)
+    SymbolKind,
+    /// The operand is a reference to a type. ($Int8, $Float, etc.) (GILType)
+    TypeKind,
+    /// The operand is a reference to a struct or enum member. (@MyStruct::field, @MyEnum::variant, etc.)
+    MemberKind,
+    /// The operand is a reference to a basic block / label. (entry, then, etc.)
+    LabelKind,
+};
+
+class GILOperand {
+    OperandKind kind;
+    union {
+        GILValue value;
+        llvm::APInt literalInt;
+        llvm::APFloat literalFloat;
+        llvm::StringRef literalString;
+        // GILFunction symbol;
+        // GILType type;
+        // GILMember member;
+        GILBasicBlock *label;
+    } data;
+
+public:
+    GILOperand(GILValue value) : kind(OperandKind::ValueKind) {
+        data.value = value;
+    }
+    GILOperand(llvm::APInt literalInt) : kind(OperandKind::LiteralIntKind) {
+        data.literalInt = literalInt;
+    }
+    GILOperand(llvm::APFloat literalFloat) : kind(OperandKind::LiteralFloatKind) {
+        data.literalFloat = literalFloat;
+    }
+    GILOperand(llvm::StringRef literalString) : kind(OperandKind::LiteralStringKind) {
+        data.literalString = literalString;
+    }
+    // GILOperand(GILFunction symbol) : kind(OperandKind::SymbolKind) {
+    //     data.symbol = symbol;
+    // }
+    // GILOperand(GILType type) : kind(OperandKind::TypeKind) {
+    //     data.type = type;
+    // }
+    // GILOperand(GILMember member) : kind(OperandKind::MemberKind) {
+    //     data.member = member;
+    // }
+    GILOperand(GILBasicBlock *label) : kind(OperandKind::LabelKind) {
+        data.label = label;
+    }
+
+    OperandKind getKind() { return kind; }
+
+    GILValue getGILValue() {
+        assert(kind == OperandKind::ValueKind && "Operand is not a value");
+        return data.value;
+    }
+    llvm::APInt getLiteralInt() {
+        assert(kind == OperandKind::LiteralIntKind && "Operand is not a literal integer");
+        return data.literalInt;
+    }
+    llvm::APFloat getLiteralFloat() {
+        assert(kind == OperandKind::LiteralFloatKind && "Operand is not a literal float");
+        return data.literalFloat;
+    }
+    llvm::StringRef getLiteralString() {
+        assert(kind == OperandKind::LiteralStringKind && "Operand is not a literal string");
+        return data.literalString;
+    }
+    // GILFunction getSymbol() {
+    //     assert(kind == OperandKind::SymbolKind && "Operand is not a symbol");
+    //     return data.symbol;
+    // }
+    // GILType getType() {
+    //     assert(kind == OperandKind::TypeKind && "Operand is not a type");
+    //     return data.type;
+    // }
+    // GILMember getMember() {
+    //     assert(kind == OperandKind::MemberKind && "Operand is not a member");
+    //     return data.member;
+    // }
+    GILBasicBlock *getLabel() {
+        assert(kind == OperandKind::LabelKind && "Operand is not a label");
+        return data.label;
+    }
+};
+
 /**
  * @class InstBase
  * @brief Represents the base class for instructions in the GIL (Glu Intermediate Language).
@@ -45,12 +139,11 @@ public:
     /// Returns the number of results produced by this instruction. For terminator
     /// instructions, this is always 0. For other instructions, this is usually 1.
     virtual size_t getResultCount() = 0;
-    /// Returns the number of operands consumed by this instruction. For literals and
-    /// other instructions that do not consume operands, this is always 0.
+    /// Returns the number of operands consumed by this instruction.
     virtual size_t getOperandCount() = 0;
     /// Returns the operand at the specified index. The index must be less than the
     /// value returned by getOperandCount().
-    virtual GILValue getOperand(size_t index) = 0;
+    virtual GILOperand getOperand(size_t index) = 0;
 
     GILValue getResult(size_t index) {
         assert(index < getResultCount() && "Result index out of range");
@@ -68,18 +161,24 @@ public:
  * @brief A class representing a conversion instruction in the GIL.
  * 
  * This class inherits from InstBase and provides functionality specific to conversion instructions,
- * which are instructions with exactly one operand and one result.
+ * which are instructions with exactly two operands (one type, one value) and one result.
  */
 class ConversionInstBase : public InstBase {
+protected:
+    // GILType destType;
     GILValue operand;
 public:
+    // GILType getDestType() { return destType; }
     GILValue getOperand() { return operand; }
 
     size_t getResultCount() override { return 1; }
-    size_t getOperandCount() override { return 1; }
-    GILValue getOperand(size_t index) override {
-        assert(index == 0 && "Invalid operand index");
-        return getOperand();
+    size_t getOperandCount() override { return 2; }
+    GILOperand getOperand(size_t index) override {
+        switch (index) {
+        case 0: // return getDestType();
+        case 1: return getOperand();
+        default: llvm_unreachable("Invalid operand index");
+        }
     }
 };
 
