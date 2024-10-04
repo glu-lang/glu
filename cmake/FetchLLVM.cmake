4dfebca9662965818790314dc3c1f168b4436cf2
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2024-10-04 10:04:41 +0900

    feat: llvm build targets filtered

--- cmake/FetchLLVM.cmake
+++ cmake/FetchLLVM.cmake
@@ -13,5 +13,9 @@ set(LLVM_BUILD_LLVM_DYLIB ON)
 set(LLVM_BUILD_DOCS OFF)
 set(LLDB_INCLUDE_TESTS OFF)
 set(LLVM_ENABLE_PROJECTS "clang;lldb")
+set(LLVM_TARGETS_TO_BUILD "AArch64;X86")
 
 FetchContent_MakeAvailable(LLVM)
+
+include_directories(${llvm_BINARY_DIR}/include)
+include_directories(${LLVM_SOURCE_DIR}/include)
