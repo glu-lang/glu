b547364d7a082ca50303bf0785dc3de325f7124c
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2024-10-08 14:16:52 +0900

    feat: code commented and simplified, no more FileEntry or FileMnger

--- include/Basic/SourceLocation.hpp
+++ include/Basic/SourceLocation.hpp
@@ -5,55 +5,51 @@
 
 namespace glu {
 
-class FileID {
-
-public:
-    bool operator==(FileID const &other) const
-    {
-        return id == other.id;
-    }
-    bool operator!=(FileID const &other) const
-    {
-        return id != other.id;
-    }
-    bool operator<(FileID const &other) const
-    {
-        return id < other.id;
-    }
-    bool operator>(FileID const &other) const
-    {
-        return id > other.id;
-    }
-    bool operator<=(FileID const &other) const
-    {
-        return id <= other.id;
-    }
-    bool operator>=(FileID const &other) const
-    {
-        return id >= other.id;
-    }
-
-private:
-    friend class SourceManager;
-
-    static FileID get(int id)
-    {
-        FileID fid;
-        fid.id = id;
-        return fid;
-    }
-
-private:
-    int id = 0;
-};
-
 class SourceManager;
 
-class SourceLocation {
+/**
+ * @class FileID
+ * @brief Contains an opaque identifier representing a file, used by the
+ * SourceManager.
+ *
+ */
+class FileID {
     friend class SourceManager;
 
 private:
-    uint32_t _offset = 0;
+    /**
+     * @brief A FileID can only be created by the SourceManager (a friend
+     * class).
+     *
+     * @param id The opaque identifier for the file.
+     * @note This constructor is private to prevent clients from using it.
+     *
+     */
+    FileID(int id)
+        : _id(id)
+    {
+    }
+
+private:
+    /// The opaque identifier for the file.
+    int _id = 0;
+};
+
+/**
+ * @class SourceLocation
+ * @brief Represents a specific location in the source code.
+ *
+ * A SourceLocation is a lightweight object that represents a specific location
+ * in the source code. It is used to refer to a specific character in a specific
+ * file. The SourceManager class is responsible for creating and interpreting
+ * SourceLocation objects.
+ *
+ * A SourceLocation is basicly an offset into the complete source code. The
+ * SourceManager knowings to which file this offset belongs can interpret it and
+ * provide useful informations from it.
+ */
+class SourceLocation {
+    friend class SourceManager;
 
 public:
     SourceLocation(uint32_t offset)
@@ -61,11 +57,11 @@ public:
     {
     }
 
-    // A SourceLocation can be invalid (ID == 0) in cases where there is no
-    // corresponding location in the source code. This typically occurs when
-    // diagnostics are generated for issues not tied to specific code, such as
-    // command-line option errors or internal compiler events. In such cases,
-    // the location cannot point to a valid file, line, or column in the code.
+    /// A SourceLocation can be invalid (ID == 0) in cases where there is no
+    /// corresponding location in the source code. This typically occurs when
+    /// diagnostics are generated for issues not tied to specific code, such as
+    /// command-line option errors or internal compiler events. In such cases,
+    /// the location cannot point to a valid file, line, or column in the code.
     bool isValid() const
     {
         return _offset != 0;
@@ -76,14 +72,18 @@ public:
     }
 
 private:
-    // The offset is an opaque value that is used to refer to a specific
-    // location in the source code. It is not intended to be used directly by
-    // clients of the library, but is instead passed to other functions that
-    // interpret it.
+    /// The offset is an opaque value that is used to refer to a specific
+    /// location in the source code. It is not intended to be used directly by
+    /// clients of the library, but is instead passed to other functions (mostly
+    /// SourceManager's functions) that interpret it.
     uint32_t getOffset() const
     {
         return _offset;
     }
+
+private:
+    /// The offset of the source location.
+    uint32_t _offset = 0;
 };
 
 }
