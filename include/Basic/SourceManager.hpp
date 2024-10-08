a740d584d942ae090ae5da3c64bf668032330c80
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2024-10-08 14:16:52 +0900

    feat: code commented and simplified, no more FileEntry or FileMnger

--- include/Basic/SourceManager.hpp
+++ include/Basic/SourceManager.hpp
@@ -5,28 +5,36 @@
 #include <llvm/Support/MemoryBuffer.h>
 #include <optional>
 
-#include "FileEntry.hpp"
 #include "SourceLocation.hpp"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/VirtualFileSystem.h"
 
-namespace glu::srcmanager {
+namespace glu {
 
-/// This object owns the MemoryBuffer object containing a file contents.
+/**
+ * @class ContentCache
+ * @brief A file loaded will be cached in this class.
+ *
+ * The ContentCache class is responsible for caching the content of a file that
+ * has been loaded. It is used by the SourceManager to store the content of a
+ * file. The content of a file is stored in a MemoryBuffer object which is owned
+ * by the ContentCache.
+ *
+ */
 class alignas(8) ContentCache {
 
 public:
-    std::optional<FileEntry> _fileEntry;
-
-public:
     ContentCache(llvm::StringRef fileName)
-        : _fileEntry(std::nullopt)
-        , _fileName(fileName)
+        : _fileName(fileName)
         , _isBufferInvalid(true)
     {
     }
+    /// Copy constructor and assignment operator are deleted.
+    /// A unique owned file content should not be copied.
     ContentCache(ContentCache const &other) = delete;
     ContentCache &operator=(ContentCache const &other) = delete;
 
+    /// Get the size of the loaded buffer if loaded.
     unsigned getSize() const
     {
         return _buffer ? (unsigned) _buffer->getBufferSize() : 0;
@@ -49,8 +57,6 @@ public:
         return std::nullopt;
     }
 
-    std::optional<llvm::MemoryBufferRef> getBufferOrNone(FileManager &fm);
-
     /// Set the MemoryBuffer for this ContentCache.
     void setBuffer(std::unique_ptr<llvm::MemoryBuffer> buffer)
     {
@@ -71,32 +77,45 @@ public:
 private:
     llvm::StringRef _fileName;
 
+    /// The buffer containing the content of the file.
     mutable std::unique_ptr<llvm::MemoryBuffer> _buffer;
+    /// A flag to indicate if the buffer is invalid.
     mutable unsigned _isBufferInvalid : 1;
 };
 
+/**
+ * @class FileInfo
+ * @brief Contains concrete information about a file.
+ *
+ * The FileInfo class contains information about a file that has been loaded by
+ * the SourceManager. It contains the SourceLocation of the import directive
+ * that brought in the file, and a reference to the ContentCache object that
+ * contains the content of the file.
+ *
+ */
 class FileInfo {
 
 public:
     FileInfo()
-        : _includeLoc(0)
+        : _importLoc(0)
         , _contentCache(nullptr) {};
 
+    /// Create a FileInfo object with the given SourceLocation and ContentCache.
     static FileInfo
     get(SourceLocation includeLoc, ContentCache &contentCache,
         llvm::StringRef fileName)
     {
         FileInfo info;
 
-        info._includeLoc = includeLoc;
+        info._importLoc = includeLoc;
         info._contentCache = &contentCache;
         contentCache.setFileName(fileName);
         return info;
     }
 
-    SourceLocation getIncludeLoc() const
+    SourceLocation getImportLoc() const
     {
-        return _includeLoc;
+        return _importLoc;
     }
 
     ContentCache const &getContentCache() const
@@ -110,60 +129,68 @@ public:
     }
 
 private:
-    SourceLocation _includeLoc;
+    /// The location of the import directive that brought in the file.
+    /// If the FileInfo is for the main file this location is invalid (ID == 0).
+    SourceLocation _importLoc;
 
-    /// The content cache and the characteristic of the file.
+    /// The content cache of the file.
     ContentCache const *_contentCache;
 };
 
+/**
+ * @class SourceLocEntry
+ * @brief Represents a specific location in the source code.
+ *
+ * A SourceLocEntry is an object that represents a specific location
+ * in the source code. It is used to refer to a specific character in a specific
+ * file. The SourceManager class is responsible for creating and interpreting
+ * SourceLocEntry objects.
+ *
+ * A SourceLocEntry is basicly an offset into the complete source code. The
+ * SourceManager knowings to which file this offset belongs can interpret it and
+ * provide useful informations from it.
+ */
 class SourceLocEntry {
 
 public:
-    SourceLocEntry()
-        : _offset()
-        , _isExpansion()
+    SourceLocEntry(uint32_t offset, FileInfo const &file)
+        : _offset(offset)
+        , _file(file)
     {
     }
 
+    /// Returnn a const reference to the FileInfo object.
     FileInfo const &getFileInfo() const
     {
         return const_cast<SourceLocEntry *>(this)->getFile();
     }
+
     FileInfo &getFile()
     {
         return _file;
     }
 
+    /// Return the offset of the source location.
     uint32_t getOffset() const
     {
         return _offset;
     }
 
-    bool isExpansion() const
-    {
-        return _isExpansion;
-    }
-
-    static SourceLocEntry get(uint32_t offset, FileInfo const &file)
-    {
-        SourceLocEntry entry;
-        entry._offset = offset;
-        entry._isExpansion = false;
-        entry._file = file;
-        return entry;
-    }
-
 private:
+    /// The offset of the source location.
     uint32_t _offset;
-    uint32_t _isExpansion : 1;
 
+    /// The file information for the source location.
     FileInfo _file;
 };
 
 class SourceManager {
-
 public:
-    SourceManager();
+    SourceManager()
+        : _vfs(llvm::vfs::getRealFileSystem())
+        , _mainFile(0)
+    {
+    }
     SourceManager(SourceManager const &other) = delete;
     SourceManager &operator=(SourceManager const &other) = delete;
     SourceManager(SourceManager &&other) = delete;
@@ -171,13 +198,45 @@ public:
 
     ~SourceManager() = default;
 
-private:
-    llvm::SmallVector<SourceLocEntry, 0> _sourceLocs;
-    std::vector<ContentCache *> MemBufferInfos;
+    llvm::ErrorOr<FileID> loadFile(llvm::StringRef filePath)
+    {
+        llvm::ErrorOr<std::unique_ptr<llvm::vfs::File>> file
+            = _vfs->openFileForRead(filePath);
 
-    FileID _mainFile;
+        if (!file) {
+            return file.getError();
+        }
 
-public:
+        llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> buffer
+            = (*file)->getBuffer(filePath);
+
+        if (!buffer) {
+            return buffer.getError();
+        }
+
+        ContentCache *cache = new ContentCache(filePath);
+        cache->setBuffer(std::move(*buffer));
+
+        _contentCaches.push_back(cache);
+
+        auto fileName = (*file)->getName();
+
+        if (!fileName) {
+            return fileName.getError();
+        }
+
+        SourceLocEntry entry(
+            _sourceLocs.size(),
+            FileInfo::get(SourceLocation(0), *cache, *fileName)
+        );
+
+        if (_sourceLocs.empty()) {
+            _mainFile = FileID(_sourceLocs.size());
+        }
+
+        return FileID(_sourceLocs.size());
+    }
+
     /// Set the file ID for the main source file.
     void setMainFileID(FileID fid)
     {
@@ -188,6 +247,13 @@ public:
     {
         return _mainFile;
     }
+
+private:
+    llvm::SmallVector<SourceLocEntry, 0> _sourceLocs;
+    std::vector<ContentCache *> _contentCaches;
+
+    llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> _vfs;
+    FileID _mainFile;
 };
 
 }
