21860f4d8fe26d7a65e22eb65a9f5126f7bb5374
LindonAliu <lindon.aliu@outlook.fr>
2024-10-10 15:51:51 +0900

    chore: delete the main test of BasicBlock list

--- tools/gluc/sources/main.cpp
+++ tools/gluc/sources/main.cpp
@@ -1,29 +1,5 @@
-#include "GIL/BasicBlock.hpp"
-#include <iostream>
+#include "IRGen/IRGen.hpp"
 
-int main()
-{
-    glu::gil::BasicBlock bb;
-    glu::gil::InstBase *inst_ptr = new glu::gil::InstBase;
-    glu::gil::InstBase *instB_ptr = new glu::gil::InstBase;
-
-    bb.addInstructionAtEnd(inst_ptr);
-    std::cout << "Instruction count: " << bb.getInstructionCount() << std::endl;
-    std::cout << "Parent of inst is: "
-              << static_cast<void *>(inst_ptr->getParent()) << std::endl;
-
-    bb.addInstructionBefore(instB_ptr, inst_ptr);
-    std::cout << "Instruction count: " << bb.getInstructionCount() << std::endl;
-    std::cout << "Parent of instB is: "
-              << static_cast<void *>(instB_ptr->getParent()) << std::endl;
-
-    glu::gil::InstBase *test = bb.popFirstInstruction();
-    std::cout << "Instruction count: " << bb.getInstructionCount() << std::endl;
-    std::cout << "Parent of test is: " << static_cast<void *>(test->getParent())
-              << std::endl;
-    delete test;
-
-    auto terminator = bb.getTerminatorInst();
-    std::cout << "Terminator: " << static_cast<void *>(terminator) << std::endl;
-    return 0;
+int main() {
+    IRGen irgen;
 }
