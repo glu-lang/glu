0bb67e393c1362d83222f4c835f6b47a7219f3dd
LindonAliu <lindon.aliu@outlook.fr>
2024-10-11 19:51:31 +0900

    feat(GIL/Function): add impl to manage the list of BasicBlock

--- include/GIL/Function.hpp
+++ include/GIL/Function.hpp
@@ -5,6 +5,7 @@
 
 namespace glu::gil {
 
+// TODO: add a return type and parameters
 class Function {
 
 public:
@@ -12,9 +13,10 @@ public:
 
 private:
     BBListType _basicBlocks;
+    std::string _name;
 
 public:
-    Function();
+    Function(const std::string &name) : _name(name) { }
     ~Function();
 
     // defined to be used by ilist
@@ -22,6 +24,28 @@ public:
     {
         return &Function::_basicBlocks;
     }
+
+    const std::string &getName() const { return _name; }
+    void setName(const std::string &name) { _name = name; }
+
+    BasicBlock *popFirstBlock();
+    BBListType const &getBasicBlocks() const { return _basicBlocks; }
+    BasicBlock *getEntryBlock() { return &_basicBlocks.front(); }
+    std::size_t getBasicBlockCount() const { return _basicBlocks.size(); }
+
+    void addBasicBlockBefore(BasicBlock *bb, BasicBlock *before);
+    void addBasicBlockAfter(BasicBlock *bb, BasicBlock *before);
+    void addBasicBlockAtEnd(BasicBlock *bb) { _basicBlocks.push_back(bb); }
+    void addBasicBlockAtStart(BasicBlock *bb) { _basicBlocks.push_front(bb); }
+    void addBasicBlockAt(BasicBlock *bb, BBListType::iterator it)
+    {
+        _basicBlocks.insert(it, bb);
+    }
+
+    void replaceBasicBlock(BasicBlock *oldBB, BasicBlock *newBB);
+
+    void removeBasicBlock(BBListType::iterator it) { _basicBlocks.erase(it); }
+    void removeBasicBlock(BasicBlock *bb) { _basicBlocks.remove(bb); }
 };
 
 } // end namespace glu::gil
