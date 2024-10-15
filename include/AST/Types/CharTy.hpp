1168fa303cc9d731aea6cc7b819c8392314c428b
LindonAliu <lindon.aliu@outlook.fr>
2024-10-15 21:51:36 +0900

    feat(AST/Types): Add CharTy class to represent the char type in the AST

---
+++ include/AST/Types/CharTy.hpp
@@ -0,0 +1,25 @@
+#ifndef CHARTY_HPP_
+#define CHARTY_HPP_
+
+#include "TypeBase.hpp"
+
+namespace glu::types {
+
+/// @brief CharTy is a class that represents the char type in the AST.
+class CharTy : public TypeBase {
+public:
+    /// @brief Constructor for the CharTy class.
+    CharTy() : TypeBase(TypeKind::CharTyKind) { }
+
+    /// @brief Static method to check if a type is a CharTy.
+    /// @param type The type to check.
+    /// @return Returns `true` if the type is a `CharTy`, `false` otherwise.
+    static bool classof(TypeBase const *type)
+    {
+        return type->getKind() == TypeKind::CharTyKind;
+    }
+};
+
+} // end namespace glu::types
+
+#endif /* !CHARTY_HPP_ */
