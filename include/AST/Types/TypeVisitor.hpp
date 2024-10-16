8cd9d941dd80fb5eec42b727242bdf84486d27ff
Marouan Bader <marouan.bader@epitech.eu>
2024-10-16 11:44:46 +0900

    feat: add typebase visitor impl

--- include/AST/Types/TypeVisitor.hpp
+++ include/AST/Types/TypeVisitor.hpp
@@ -1,6 +1,52 @@
-#ifndef GLU_AST_TYPES_TYPEVISITOR_HPP_
-#define GLU_AST_TYPES_TYPEVISITOR_HPP_
+#ifndef GLU_AST_TYPES_TYPEVISITOR_HPP
+#define GLU_AST_TYPES_TYPEVISITOR_HPP
 
-class TypeVisitor { };
+#include "TypeBase.hpp"
+#include <llvm/Support/ErrorHandling.h>
 
-#endif
+namespace glu::types {
+
+template <typename Impl, typename RetTy = void, typename... ArgTys>
+class TypeVisitor {
+    Impl *asImpl() { return static_cast<Impl *>(this); }
+
+public:
+    /**
+     * @brief Visit a Type.
+     *
+     * This function call the appropriate visit function for the type's kind.
+     *
+     * @param type The type to visit.
+     * @param ...args The arguments to pass to the visit function.
+     * @return The value returned by the visit function.
+     *
+     **/
+    RetTy visit(TypeBase *type, ArgTys... args)
+    {
+        return _visitType(type, std::forward<ArgTys>(args)...);
+    }
+
+    RetTy _visitType(TypeBase *type, ArgTys... args)
+    {
+        switch (type->getKind()) {
+#define TYPE(NAME)                                                     \
+case TypeKind::NAME##Kind:                                             \
+    return asImpl()->visit##NAME(type, std::forward<ArgTys>(args)...);
+#include "TypeKind.def"
+        default: llvm_unreachable("Unknown type kind.");
+        }
+    }
+
+    RetTy visitTypeBase(TypeBase *type, ArgTys... args) { }
+
+#define TYPE(NAME)                                                         \
+    RetTy visit##NAME(TypeBase *type, ArgTys... args)                      \
+    {                                                                      \
+        return asImpl()->visit##NAME(type, std::forward<ArgTys>(args)...); \
+    }
+#include "TypeKind.def"
+};
+
+} // namespace glu::types
+
+#endif /* !GLU_AST_TYPES_TYPEVISITOR_HPP */
