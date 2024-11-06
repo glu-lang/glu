625c66c6a699ba263181c3e68920a4604c66d5a7
LindonAliu <lindon.aliu@outlook.fr>
2024-11-06 17:24:34 +0900

    feat(AST/Types): add UnresolvedNameTy class to represent unresolved name types

---
+++ include/AST/Types/UnresolvedNameTy.hpp
@@ -0,0 +1,29 @@
+#ifndef GLU_AST_TYPES_UNRESOLVEDNAMETY_HPP
+#define GLU_AST_TYPES_UNRESOLVEDNAMETY_HPP
+
+#include <string>
+
+namespace glu::types {
+
+/// @brief UnresolvedNameTy is a class that represents an unresolved name type
+/// in the AST.
+class UnresolvedNameTy : public TypeBase {
+    std::string const _name;
+
+public:
+    UnresolvedNameTy(std::string name)
+        : TypeBase(TypeKind::UnresolvedNameTyKind), _name(std::move(name))
+    {
+    }
+
+    ~UnresolvedNameTy();
+
+    static bool classof(TypeBase const *type)
+    {
+        return type->getKind() == TypeKind::UnresolvedNameTyKind;
+    }
+};
+
+}
+
+#endif // GLU_AST_TYPES_UNRESOLVEDNAMETY_HPP
