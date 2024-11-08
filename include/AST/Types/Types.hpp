735d4f5a31d6809a0233c8b9e44509566d4dd2fa
Marouan Bader <marouan.bader@epitech.eu>
2024-11-08 16:10:26 +0900

    feat: add new static and dynamic arrays classes to Types.hpp

--- include/AST/Types/Types.hpp
+++ include/AST/Types/Types.hpp
@@ -1,11 +1,12 @@
 #ifndef GLU_AST_TYPES_TYPES_HPP
 #define GLU_AST_TYPES_TYPES_HPP
 
-#include "ArrayTy.hpp"
 #include "BoolTy.hpp"
 #include "CharTy.hpp"
+#include "DynamicArrayTy.hpp"
 #include "EnumTy.hpp"
 #include "FunctionTy.hpp"
+#include "StaticArrayTy.hpp"
 #include "StructTy.hpp"
 #include "UnresolvedNameTy.hpp"
 
