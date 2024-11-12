1df58cd1eb196d97995afcaa7070d48a6397c099
Marouan Bader <marouan.bader@epitech.eu>
2024-11-12 13:42:04 +0900

    chore: restore main to original version

--- tools/gluc/sources/main.cpp
+++ tools/gluc/sources/main.cpp
@@ -1,17 +1,6 @@
-#include "GIL/Function.hpp"
-#include "GIL/Module.hpp"
 #include "IRGen/IRGen.hpp"
-#include <iostream>
 
 int main() {
-    glu::gil::Module module("test");
-
-    module.addFunction("main", nullptr, {});
-    module.addFunction("foo", nullptr, {});
-    module.addFunction("bar", nullptr, {});
-    std::cout << module.getName() << std::endl;
-    for (auto &f : module.getFunctions())
-        std::cout << f.getName() << std::endl;
-    // module.clearFunctions();
+    IRGen irgen;
     return 0;
 }
