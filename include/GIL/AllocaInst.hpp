2e68bf3ffb242843e9f800422525df78bd0e44c4
Marouan Bader <marouan.bader@epitech.eu>
2024-11-12 15:56:58 +0900

    feat: add base AlocaInst prototyped class

---
+++ include/GIL/AllocaInst.hpp
@@ -0,0 +1,12 @@
+#ifndef GLU_GIL_ALLOCAINST_HPP
+#define GLU_GIL_ALLOCAINST_HPP
+
+#include "InstBase.hpp"
+
+namespace glu::gil {
+
+class AllocaInst : public InstBase { };
+
+} // end namespace glu::gil
+
+#endif // GLU_GIL_ALLOCAINST_HPP
