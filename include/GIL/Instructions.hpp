156af704a9d191206537ec0f11f450d8f757f03b
Emil Pedersen <emil.pedersen@epitech.eu>
2024-11-16 19:12:42 -0500

    feat: call instruction

--- include/GIL/Instructions.hpp
+++ include/GIL/Instructions.hpp
@@ -2,6 +2,7 @@
 #ifndef GLU_GIL_INSTRUCTIONS_HPP
 #define GLU_GIL_INSTRUCTIONS_HPP
 
+#include "Instructions/CallInst.hpp"
 #include "Instructions/FloatLiteralInst.hpp"
 #include "Instructions/IntegerLiteralInst.hpp"
 #include "Instructions/StringLiteralInst.hpp"
