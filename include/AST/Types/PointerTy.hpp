4c63a6f6d1247f0c6b37c3ff63bc57e1ddb03dfb
yunsecode <yunse.lee@gmail.com>
2024-11-19 23:13:51 +0900

    feat: set the type in constructor

--- include/AST/Types/PointerTy.hpp
+++ include/AST/Types/PointerTy.hpp
@@ -7,9 +7,15 @@ namespace glu::types {
 
 /// @brief Pointer is a class that represents the pointer type in the AST.
 class PointerTy : public TypeBase {
+private:
+    TypeBase * const _type;
+
 public:
     /// @brief Constructor for the Pointer class.
-    PointerTy() : TypeBase(TypeKind::PointerTyKind) { }
+    /// @param type The type this pointer points to.
+    PointerTy(TypeBase *type) : TypeBase(TypeKind::PointerTyKind), _type(type)
+    {
+    }
 
     /// @brief Static method to check if a type is a PointerTy.
     /// @param type The type to check.
@@ -18,7 +24,12 @@ public:
     {
         return type->getKind() == TypeKind::PointerTyKind;
     }
+
+    /// @brief Getter for the type this pointer points to.
+    /// @return Returns the type this pointer points to.
+    TypeBase *getType() const { return _type; }
 };
+
 } // end namespace glu::types
 
 #endif // GLU_AST_TYPES_POINTERTY_HPP
