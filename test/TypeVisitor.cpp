ddb41131e0c902ad5d7ee785d0ea5c3e94695ead
Marouan Bader <marouan.bader@epitech.eu>
2024-11-19 21:46:38 +0900

    chore: use TEST_F instead of TEST in TypeVisitor unit tests

--- test/TypeVisitor.cpp
+++ test/TypeVisitor.cpp
@@ -21,8 +21,13 @@ public:
     }
 };
 
+class TestVisitorFixture : public ::testing::Test {
+protected:
+    TestVisitor visitor;
+};
+
 // Tests the IntTy visit function
-TEST(TestVisitor, VisitIntTy)
+TEST_F(TestVisitorFixture, VisitIntTy)
 {
     TestVisitor visitor;
     glu::types::IntTy intType(glu::types::IntTy::Signed, 4);
@@ -31,7 +36,7 @@ TEST(TestVisitor, VisitIntTy)
 }
 
 // Tests the BoolTy visit function
-TEST(TestVisitor, VisitBoolTy)
+TEST_F(TestVisitorFixture, VisitBoolTy)
 {
     TestVisitor visitor;
     glu::types::BoolTy boolType;
@@ -40,7 +45,7 @@ TEST(TestVisitor, VisitBoolTy)
 }
 
 // Tests the CharTy visit function
-TEST(TestVisitor, VisitCharTy)
+TEST_F(TestVisitorFixture, VisitCharTy)
 {
     TestVisitor visitor;
     glu::types::CharTy charType;
