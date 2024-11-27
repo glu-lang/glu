18ddd405990928ffc5ec195b70864715040c0e2b
LindonAliu <lindon.aliu@outlook.fr>
2024-11-28 03:11:20 +0900

    tests: add assertion for parameter count in FunctionDecl constructor test

--- test/AST/Decl/FunctionDecl.cpp
+++ test/AST/Decl/FunctionDecl.cpp
@@ -33,6 +33,7 @@ TEST_F(FunctionDeclTest, FunctionDeclConstructor)
     ASTNode *test = &decl;
     ASSERT_EQ(decl.getName(), name);
     ASSERT_EQ(decl.getType(), &type);
+    ASSERT_EQ(decl.getParams().size(), 2);
     ASSERT_TRUE(decl.getBody().getStmts().empty());
     ASSERT_TRUE(llvm::isa<FunctionDecl>(test));
     ASSERT_FALSE(llvm::isa<StmtBase>(&decl));
