58bba48b837a1058251a307c6ef90d6e06e55c06
LindonAliu <lindon.aliu@outlook.fr>
2024-11-27 23:08:29 +0900

    feat(AST): add VarDecl class and corresponding tests

---
+++ test/AST/Decl/VarDecl.cpp
@@ -0,0 +1,35 @@
+#include "Decl/VarDecl.hpp"
+#include "ASTNode.hpp"
+#include "Types/Types.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace glu::ast;
+using namespace glu::types;
+
+class TestExprBase : public ExprBase {
+public:
+    TestExprBase()
+        : ExprBase(NodeKind::LiteralExprKind, glu::SourceLocation(1), nullptr)
+    {
+    }
+};
+
+TEST(VarDeclTest, Constructor)
+{
+    glu::SourceLocation loc(1);
+    std::string name = "x";
+    TypeBase *type = new BoolTy();
+    ExprBase *value = new TestExprBase();
+
+    VarDecl varDecl(loc, name, type, value);
+
+    EXPECT_EQ(varDecl.getName(), name);
+    EXPECT_EQ(varDecl.getType(), type);
+    EXPECT_EQ(varDecl.getValue(), value);
+    EXPECT_TRUE(llvm::isa<DeclBase>(varDecl));
+    EXPECT_EQ(varDecl.getValue()->getParent(), &varDecl);
+
+    delete type;
+    delete value;
+}
