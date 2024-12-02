d00abfcd3718b8da6faa2b64438558032f7420e5
Marouan Bader <marouan.bader@epitech.eu>
2024-12-02 19:46:50 +0900

    chore: replace Type placeholder with real Type implementation

--- include/GIL/Instructions/InstBase.hpp
+++ include/GIL/Instructions/InstBase.hpp
@@ -8,7 +8,7 @@
 #include <llvm/ADT/ilist.h>
 #include <llvm/ADT/ilist_node.h>
 #include <llvm/Support/Casting.h>
-#include "Type.h"
+#include "Type.hpp"
 
 #include "Basic/SourceLocation.hpp"
 #include "Member.hpp"
