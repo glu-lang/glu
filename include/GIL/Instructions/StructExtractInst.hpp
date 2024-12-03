024bc34fc62f1425b8e21df53e67e19cbd73014c
Marouan Bader <marouan.bader@epitech.eu>
2024-12-03 15:19:59 +0900

    style: change comments style in StructExtractInst class

--- include/GIL/Instructions/StructExtractInst.hpp
+++ include/GIL/Instructions/StructExtractInst.hpp
@@ -5,25 +5,25 @@
 
 namespace glu::gil {
 
-/**
- * @class StructExtractInst
- * @brief Represents an instruction to extract a member from a structure.
- *
- * This class is derived from InstBase and represents an instruction to extract
- * a member from a structure in the GLU GIL (Generic Intermediate Language).
- */
+///
+/// @class StructExtractInst
+/// @brief Represents an instruction to extract a member from a structure.
+///
+/// This class is derived from InstBase and represents an instruction to extract
+/// a member from a structure in the GLU GIL (Generic Intermediate Language).
+///
 class StructExtractInst : public InstBase {
     Value
         structValue; ///< The structure value from which to extract the member.
     Member member; ///< The member to extract from the structure.
 
 public:
-    /**
-     * @brief Constructs a StructExtractInst object.
-     *
-     * @param structValue The structure value from which to extract the member.
-     * @param member The member to extract from the structure.
-     */
+    ///
+    /// @brief Constructs a StructExtractInst object.
+    ///
+    /// @param structValue The structure value from which to extract the member.
+    /// @param member The member to extract from the structure.
+    ///
     StructExtractInst(Value structValue, Member member)
         : InstBase(InstKind::StructExtractInstKind)
         , structValue(structValue)
@@ -31,18 +31,18 @@ public:
     {
     }
 
-    /**
-     * @brief Gets the structure value.
-     *
-     * @return The structure value.
-     */
+    ///
+    /// @brief Gets the structure value.
+    ///
+    /// @return The structure value.
+    ///
     Value getStructValue() const { return structValue; }
 
-    /**
-     * @brief Gets the member to extract.
-     *
-     * @return The member to extract.
-     */
+    ///
+    /// @brief Gets the member to extract.
+    ///
+    /// @return The member to extract.
+    ///
     Member getMember() const { return member; }
 
     // TODO: Implement getMemberType() method when Memeber will be defined.
