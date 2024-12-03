b5055c72e0e360a929721c81fd7300b534a2a115
Marouan Bader <marouan.bader@epitech.eu>
2024-12-03 15:18:21 +0900

    style: change comments style

--- include/GIL/Instructions/AllocaInst.hpp
+++ include/GIL/Instructions/AllocaInst.hpp
@@ -5,40 +5,40 @@
 
 namespace glu::gil {
 
-/**
- * @class AllocaInst
- * @brief Represents an allocation instruction.
- *
- * This class is derived from InstBase and represents an allocation instruction
- * in the GLU GIL (Generic Intermediate Language).
- */
+///
+///  @class AllocaInst
+///  @brief Represents an allocation instruction.
+///
+///  This class is derived from InstBase and represents an allocation instruction
+///  in the GLU GIL (Generic Intermediate Language).
+///
 class AllocaInst : public InstBase {
     size_t size; ///< The size of the allocation.
     Type type; ///< The type of the allocation.
 public:
-    /**
-     * @brief Constructs an AllocaInst object.
-     *
-     * @param size The size of the allocation.
-     * @param type The type of the allocation.
-     */
+    ///
+    /// @brief Constructs an AllocaInst object.
+    ///
+    /// @param size The size of the allocation.
+    /// @param type The type of the allocation.
+    ///
     AllocaInst(size_t size, Type type)
         : InstBase(InstKind::AllocaInstKind), size(size), type(type)
     {
     }
 
-    /**
-     * @brief Gets the size of the allocation.
-     *
-     * @return The size of the allocation.
-     */
+    ///
+    /// @brief Gets the size of the allocation.
+    ///
+    /// @return The size of the allocation.
+    ///
     size_t getSize() const { return size; }
 
-    /**
-     * @brief Gets the type of the allocation.
-     *
-     * @return The type of the allocation.
-     */
+    ///
+    /// @brief Gets the type of the allocation.
+    ///
+    /// @return The type of the allocation.
+    ///
     Type getType() const { return type; }
 };
 
