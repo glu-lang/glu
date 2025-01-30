67d78beffd408f239a55606ce871111cfacea285
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2025-01-30 06:52:00 -0500

    feat: token names shortened and error token added

--- include/Basic/Tokens.hpp
+++ include/Basic/Tokens.hpp
@@ -7,38 +7,26 @@ namespace glu {
 enum class TokenKind {
 #define TOKEN(Name) Name##Tok,
 #include "TokenKind.def"
-    TOKEN_NUM
+    numberOfTok
 };
 
 class Token {
     /// The kind of the token
     TokenKind _kind;
 
-    /// Whether the token is the first token on a line.
-    unsigned _atStartOfLine : 1;
-
     /// The actual lexeme of the token
     llvm::StringRef _lexeme;
 
 public:
-    Token(TokenKind kind, llvm::StringRef lexeme, unsigned commentLength = 0)
-        : _kind(kind), _atStartOfLine(false), _lexeme(lexeme)
+    Token(TokenKind kind, llvm::StringRef lexeme) : _kind(kind), _lexeme(lexeme)
     {
     }
-
-    Token() : Token(TokenKind::TOKEN_NUM, {}, 0) { }
 
     TokenKind getKind() const { return _kind; }
     void setKind(TokenKind kind) { _kind = kind; }
 
     bool is(TokenKind kind) const { return _kind == kind; }
     bool isNot(TokenKind kind) const { return _kind != kind; }
-
-    bool isAtStartOfLine() const { return _atStartOfLine; }
-    void setAtStartOfLine(bool atStartOfLine)
-    {
-        _atStartOfLine = atStartOfLine;
-    }
 
     /// True if the token is any keyword.
     bool isKeyword() const
