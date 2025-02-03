5f962f89d528dddc1aa1f055debd784771c883c0
LindonAliu <lindon.aliu@outlook.fr>
2025-02-03 21:49:43 +0100

    feat(AST): add header file that iclude all files of the folder

---
+++ include/AST/Decl/Decls.hpp
@@ -0,0 +1,8 @@
+#ifndef GLU_AST_DECL_DECLS_HPP
+#define GLU_AST_DECL_DECLS_HPP
+
+#include "FunctionDecl.hpp"
+#include "LetDecl.hpp"
+#include "VarDecl.hpp"
+
+#endif // GLU_AST_DECL_DECLS_HPP
---
+++ include/AST/Stmt/Stmts.hpp
@@ -0,0 +1,10 @@
+#ifndef GLU_AST_STMT_STMTS_HPP
+#define GLU_AST_STMT_STMTS_HPP
+
+#include "BreakStmt.hpp"
+#include "CompoundStmt.hpp"
+#include "ContinueStmt.hpp"
+#include "ReturnStmt.hpp"
+#include "WhileStmt.hpp"
+
+#endif // GLU_AST_STMT_STMTS_HPP
