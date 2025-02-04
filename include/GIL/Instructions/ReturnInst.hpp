ec6e014cc538dd93023ab59d5be7f7de4a873bf4
yunsecode <yunsecode@gmail.com>
2025-02-05 01:19:06 +0900

    chore: change class name to unreachable

--- include/GIL/Instructions/ReturnInst.hpp
+++ include/GIL/Instructions/ReturnInst.hpp
@@ -1,26 +1,26 @@
-#ifndef GLU_GIL_INSTRUCTIONS_UNREACHABLE_INST_HPP
-#define GLU_GIL_INSTRUCTIONS_UNREACHABLE_INST_HPP
+#ifndef GLU_GIL_INSTRUCTIONS_RETURN_INST_HPP
+#define GLU_GIL_INSTRUCTIONS_RETURN_INST_HPP
 
 #include "TerminatorInst.hpp"
 
 namespace glu::gil {
-/// @class UnreachableInst
+/// @class ReturnInst
 /// @brief Represents a return instruction in the GIL.
 ///
 /// This instruction is a control flow terminator, meaning it marks the end of
 /// execution in a function. It does not produce any results and must always be
 /// the last instruction in a basic block.
-class UnreachableInst : public TerminatorInst {
+class ReturnInst : public TerminatorInst {
 public:
-    UnreachableInst(Value value)
-        : TerminatorInst(InstKind::UnreachableInstKind), value(value)
+    ReturnInst(Value value)
+        : TerminatorInst(InstKind::ReturnInstKind), value(value)
     {
         // TODO: assert(llvm::isa<glu::types::PointerTy>(*value.getType()));
     }
 
     static bool classof(TerminatorInst const *inst)
     {
-        return inst->getKind() == InstKind::UnreachableInstKind;
+        return inst->getKind() == InstKind::ReturnInstKind;
     }
 
     size_t getOperandCount() const override { return 1; }
@@ -32,4 +32,4 @@ public:
 
 } // end namespace glu::gil
 
-#endif // GLU_GIL_INSTRUCTIONS_UNREACHABLE_INST_HPP
+#endif // GLU_GIL_INSTRUCTIONS_RETURN_INST_HPP
