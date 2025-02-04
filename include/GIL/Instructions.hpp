4c162fb157e13f8f390147c8144fee84d6f6ce69
yunsecode <yunsecode@gmail.com>
2025-02-05 01:24:44 +0900

    feat: implement returnInst

--- include/GIL/Instructions.hpp
+++ include/GIL/Instructions.hpp
@@ -11,6 +11,7 @@
 #include "Instructions/IntTruncInst.hpp"
 #include "Instructions/IntegerLiteralInst.hpp"
 #include "Instructions/LoadInst.hpp"
+#include "Instructions/ReturnInst.hpp"
 #include "Instructions/StringLiteralInst.hpp"
 #include "Instructions/StructExtractInst.hpp"
 #include "Instructions/UnreachableInst.hpp"
