66000758e5b11c1092a9d30f2683c7e39baff2b3
Marouan Bader <marouan.bader@epitech.eu>
2025-02-04 18:49:25 +0700

    feat: add ForStmt in Stmts.hpp

--- include/AST/Stmts.hpp
+++ include/AST/Stmts.hpp
@@ -5,6 +5,7 @@
 #include "Stmt/CompoundStmt.hpp"
 #include "Stmt/ContinueStmt.hpp"
 #include "Stmt/ExpressionStmt.hpp"
+#include "Stmt/ForStmt.hpp"
 #include "Stmt/ReturnStmt.hpp"
 #include "Stmt/WhileStmt.hpp"
 
