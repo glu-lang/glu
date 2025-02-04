f000680b7b75296ecbb4ad1b378a35001c2821f7
LindonAliu <lindon.aliu@outlook.fr>
2025-02-04 01:26:03 +0100

    feat(AST): add ExpressionStmt

--- include/AST/Stmts.hpp
+++ include/AST/Stmts.hpp
@@ -4,6 +4,7 @@
 #include "Stmt/BreakStmt.hpp"
 #include "Stmt/CompoundStmt.hpp"
 #include "Stmt/ContinueStmt.hpp"
+#include "Stmt/ExpressionStmt.hpp"
 #include "Stmt/ReturnStmt.hpp"
 #include "Stmt/WhileStmt.hpp"
 
