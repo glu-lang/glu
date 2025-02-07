fc7d21b9c19f41fb9b46f036beb992befcb2db79
LindonAliu <lindon.aliu@outlook.fr>
2025-02-07 23:15:52 +0100

    feat(ast): add TypesMemoryArena for type node memory allocation and update ASTContext

--- include/AST/ASTContext.hpp
+++ include/AST/ASTContext.hpp
@@ -2,11 +2,13 @@
 #define GLU_AST_ASTCONTEXT_HPP
 
 #include "ASTMemoryArena.hpp"
+#include "TypesMemoryArena.hpp"
 
 namespace glu::ast {
 
 class ASTContext {
     ASTMemoryArena _astMemoryArena;
+    TypesMemoryArena _typesMemoryArena;
 
 public:
     ASTContext() = default;
@@ -14,6 +16,10 @@ public:
     /// @brief Get the memory arena used by the AST context.
     /// @return The memory arena used by the AST context.
     ASTMemoryArena &getASTMemoryArena() { return _astMemoryArena; }
+
+    /// @brief Get the memory arena used by the types in the AST context.
+    /// @return The memory arena used by the types in the AST context.
+    TypesMemoryArena &getTypesMemoryArena() { return _typesMemoryArena; }
 };
 
 }; // namespace glu::ast
--- test/AST/ASTContext.cpp
+++ test/AST/ASTContext.cpp
@@ -1,6 +1,7 @@
 #include "AST/ASTContext.hpp"
 #include "AST/ASTNode.hpp"
-#include "AST/Stmt/ReturnStmt.hpp"
+#include "AST/Stmts.hpp"
+#include "AST/Types.hpp"
 #include "Basic/SourceLocation.hpp"
 
 #include <llvm/Support/Casting.h>
@@ -8,6 +9,7 @@
 #include <gtest/gtest.h>
 
 using namespace glu::ast;
+using namespace glu::types;
 
 TEST(ASTContextTest, ASTContextTest)
 {
@@ -15,7 +17,9 @@ TEST(ASTContextTest, ASTContextTest)
     glu::SourceLocation loc(11);
 
     auto stmt = ctx.getASTMemoryArena().createNode<ReturnStmt>(loc, nullptr);
+    auto type = ctx.getTypesMemoryArena().createNode<BoolTy>();
 
     ASSERT_NE(stmt, nullptr);
     ASSERT_TRUE(llvm::isa<ReturnStmt>(stmt));
+    ASSERT_TRUE(llvm::isa<BoolTy>(type));
 }
