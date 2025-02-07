2abeb9db02cfb6382cfc4a96a8fd08af648d0b3a
LindonAliu <lindon.aliu@outlook.fr>
2025-02-07 23:48:54 +0100

    doc: enhance TypedMemoryArena with detailed documentation and update test case name for clarity

--- include/Basic/TypedMemoryArena.hpp
+++ include/Basic/TypedMemoryArena.hpp
@@ -6,8 +6,24 @@
 
 namespace glu {
 
+/// @brief A memory arena that ensures allocated objects are subclasses of a
+/// specified base class.
+///
+/// @tparam Base The base class that all allocated objects must inherit from.
 template <typename Base> class TypedMemoryArena : public MemoryArena {
 public:
+    /// @brief Creates and allocates an object of type T within the memory
+    /// arena.
+    ///
+    /// @tparam T The type of the object to create. Must be a subclass of Base.
+    /// @tparam Args The types of the arguments to pass to the constructor of T.
+    /// @param args The arguments to pass to the constructor of T.
+    /// @return A pointer to the newly created object of type T.
+    ///
+    /// @note This function uses static_assert to ensure that T is a subclass of
+    /// Base.
+    /// @note This function forwards the provided arguments to the constructor
+    /// of T.
     template <typename T, typename... Args> T *create(Args &&...args)
     {
         static_assert(
