59cd964b16096f77083981940d331e9eb72f7633
Marouan Bader <marouan.bader@epitech.eu>
2025-02-21 16:45:25 +0700

    feat: add visit methods for stmts and unit tests

--- include/AST/ASTPrinter.hpp
+++ include/AST/ASTPrinter.hpp
@@ -2,6 +2,7 @@
 #define GLU_AST_PRINTER_HPP
 
 #include "ASTVisitor.hpp"
+#include "Stmts.hpp"
 
 #include <llvm/Support/raw_ostream.h>
 
@@ -21,21 +22,58 @@ case NodeKind::Name##Kind: return out << #Name;
     }
 }
 
-class ASTPrinter : public ASTVisitor<ASTPrinter, void> {
-    llvm::raw_ostream &out;
+/// @class ASTPrinter
+/// @brief A class that prints the Abstract Syntax Tree (AST) nodes.
+///
+/// This class is derived from ASTVisitor and is used to print the AST nodes
+/// to the specified output stream.
+class ASTPrinter : public ASTVisitor<ASTPrinter> {
+    llvm::raw_ostream &out; ///< The output stream to print the AST nodes.
+    size_t _indent = 0; ///< The current indentation level.
+
+    /// @brief Called before visiting an AST node.
+    void beforeVisit(ASTNode *node);
+
+    /// @brief Called after visiting an AST node.
+    void afterVisit();
 
 public:
+    /// @brief Constructs an ASTPrinter object.
+    /// @param out The output stream to print the AST nodes. Defaults to
+    /// llvm::outs().
     ASTPrinter(llvm::raw_ostream &out = llvm::outs()) : out(out) { }
 
-    void visitASTNode(ASTNode *node)
-    {
-        if (!node) {
-            out << "Null ASTNode\n";
-            return;
-        }
-        out << node->getKind()
-            << " at loc : " << node->getLocation().getOffset() << "\n";
-    }
+    /// @brief Visits an AST node.
+    /// @param node The AST node to be visited.
+    void visitASTNode(ASTNode *node);
+
+    /// @brief Visits a BreakStmt node.
+    /// @param node The BreakStmt node to be visited.
+    void visitBreakStmt(BreakStmt *node);
+
+    /// @brief Visits a CompoundStmt node.
+    /// @param node The CompoundStmt node to be visited.
+    void visitCompoundStmt(CompoundStmt *node);
+
+    /// @brief Visits a ContinueStmt node.
+    /// @param node The ContinueStmt node to be visited.
+    void visitContinueStmt(ContinueStmt *node);
+
+    /// @brief Visits an ExpressionStmt node.
+    /// @param node The ExpressionStmt node to be visited.
+    void visitExpressionStmt(ExpressionStmt *node);
+
+    /// @brief Visits an IfStmt node.
+    /// @param node The IfStmt node to be visited.
+    void visitIfStmt(IfStmt *node);
+
+    /// @brief Visits a ReturnStmt node.
+    /// @param node The ReturnStmt node to be visited.
+    void visitReturnStmt(ReturnStmt *node);
+
+    /// @brief Visits a WhileStmt node.
+    /// @param node The WhileStmt node to be visited.
+    void visitWhileStmt(WhileStmt *node);
 };
 
 } // namespace glu::ast
--- lib/ASTPrinter/ASTPrinter.cpp
+++ lib/ASTPrinter/ASTPrinter.cpp
@@ -1,1 +1,91 @@
 #include "ASTPrinter.hpp"
+
+namespace glu::ast {
+
+void ASTPrinter::beforeVisit(ASTNode *node)
+{
+    out.indent(_indent);
+    out << node->getKind() << " at loc : " << node->getLocation().getOffset();
+    _indent += 2;
+}
+
+void ASTPrinter::afterVisit()
+{
+    _indent -= 2;
+}
+
+void ASTPrinter::visitASTNode(ASTNode *node)
+{
+    if (!node) {
+        out.indent(_indent);
+        out << "Null ASTNode\n";
+    } else {
+        beforeVisit(node);
+        out << "\n";
+        afterVisit();
+    }
+}
+
+void ASTPrinter::visitIfStmt(IfStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    visit(node->getCondition());
+    visit(node->getBody());
+    visit(node->getElse());
+    afterVisit();
+}
+
+void ASTPrinter::visitBreakStmt(BreakStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    afterVisit();
+}
+
+void ASTPrinter::visitCompoundStmt(CompoundStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    for (auto stmt : node->getStatements()) {
+        visit(stmt);
+    }
+    afterVisit();
+}
+
+void ASTPrinter::visitContinueStmt(ContinueStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    afterVisit();
+}
+
+void ASTPrinter::visitExpressionStmt(ExpressionStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    if (!node->getExpr()) {
+        assert(false && "ExpressionStmt has no expression.");
+    }
+    visit(node->getExpr());
+    afterVisit();
+}
+
+void ASTPrinter::visitReturnStmt(ReturnStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    visit(node->getReturnExpr());
+    afterVisit();
+}
+
+void ASTPrinter::visitWhileStmt(WhileStmt *node)
+{
+    beforeVisit(node);
+    out << "\n";
+    visit(node->getCondition());
+    visit(node->getBody());
+    afterVisit();
+}
+
+} // namespace glu::ast
