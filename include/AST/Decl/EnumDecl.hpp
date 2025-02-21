ca7344c46fdeaa01fc56985820fa8ef24d0bf1e8
LindonAliu <lindon.aliu@outlook.fr>
2025-02-22 00:32:26 +0100

    refactor(types): replace std::string with llvm::StringRef and update case/field types in EnumDecl and StructDecl

--- include/AST/Decl/EnumDecl.hpp
+++ include/AST/Decl/EnumDecl.hpp
@@ -16,6 +16,7 @@ namespace glu::ast {
 /// declaration.
 class EnumDecl : public DeclBase {
     using EnumTy = glu::types::EnumTy;
+    using Case = glu::types::Case;
     EnumTy *_self;
 
 public:
@@ -28,7 +29,7 @@ public:
     /// the enum.
     EnumDecl(
         ASTContext &context, SourceLocation location, ASTNode *parent,
-        std::string name, llvm::SmallVector<EnumTy::Case> cases
+        std::string name, llvm::SmallVector<Case> cases
     )
         : DeclBase(NodeKind::EnumDeclKind, location, parent)
         , _self(context.getTypesMemoryArena().create<EnumTy>(
@@ -39,7 +40,7 @@ public:
 
     /// @brief Getter for the name of the enum.
     /// @return Returns the name of the enum.
-    std::string getName() const { return _self->getName(); }
+    llvm::StringRef getName() const { return _self->getName(); }
 
     /// @brief Getter for the type of this enum.
     /// @return Returns the type of this enum.
