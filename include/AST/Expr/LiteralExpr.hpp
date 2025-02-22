9e8e60599e4c3fbfc18700d7496d0fd686347967
Marouan Bader <marouan.bader@epitech.eu>
2025-02-22 18:33:53 +0700

    feat: add variant that stores the value in LiteralExpr class

--- include/AST/Expr/LiteralExpr.hpp
+++ include/AST/Expr/LiteralExpr.hpp
@@ -3,15 +3,26 @@
 
 #include "ASTNode.hpp"
 #include "Types.hpp"
+#include <llvm/ADT/APFloat.h>
+#include <llvm/ADT/APInt.h>
+#include <llvm/ADT/StringRef.h>
+#include <variant>
 
 namespace glu::ast {
 
 class LiteralExpr : public ExprBase {
+    using LiteralValue
+        = std::variant<llvm::APInt, llvm::APFloat, llvm::StringRef, bool>;
+    LiteralValue _value;
     glu::types::TypeBase *_type;
 
 public:
-    LiteralExpr(glu::types::TypeBase *type, SourceLocation loc)
-        : ExprBase(NodeKind::LiteralExprKind, loc), _type(type) { };
+    LiteralExpr(
+        LiteralValue value, glu::types::TypeBase *type, SourceLocation loc
+    )
+        : ExprBase(NodeKind::LiteralExprKind, loc)
+        , _value(value)
+        , _type(type) { };
 
     static bool classof(ASTNode const *node)
     {
@@ -19,6 +30,8 @@ public:
     }
 
     glu::types::TypeBase *getType() { return _type; }
+
+    LiteralValue getValue() { return _value; }
 };
 
 }
