b0d56759c5f967f1b43c6739b24b7282a82c73b9
Emil Pedersen <emil.pedersen@epitech.eu>
2025-02-24 17:31:59 -0500

    refactor: rename PointerDeref to UnaryOp, add doc comments

--- include/AST/Expr/StructMemberExpr.hpp
+++ include/AST/Expr/StructMemberExpr.hpp
@@ -1,5 +1,5 @@
-#ifndef GLU_AST_EXPR_STRUCT_MEMBER_EXPR_KIND_HPP
-#define GLU_AST_EXPR_STRUCT_MEMBER_EXPR_KIND_HPP
+#ifndef GLU_AST_EXPR_STRUCT_MEMBER_EXPR_HPP
+#define GLU_AST_EXPR_STRUCT_MEMBER_EXPR_HPP
 
 #include "ASTNode.hpp"
 
@@ -7,11 +7,17 @@
 
 namespace glu::ast {
 
+/// @brief Represents a struct member access expression in the AST (e.g.,
+/// val.member).
 class StructMemberExpr : public ExprBase {
     ExprBase *_value;
     llvm::StringRef _memberName;
 
 public:
+    /// @brief Constructs a StructMemberExpr.
+    /// @param loc the source location of the dot token
+    /// @param value the value, of a struct type, to access the member from
+    /// @param memberName the name of the member to access
     StructMemberExpr(
         SourceLocation loc, ExprBase *value, llvm::StringRef memberName
     )
@@ -21,8 +27,10 @@ public:
     {
     }
 
+    /// @brief Returns the expression representing the struct.
     ExprBase *getStructExpr() const { return _value; }
 
+    /// @brief Returns the name of the member to access.
     llvm::StringRef getMemberName() const { return _memberName; }
 
     static bool classof(ASTNode const *node)
@@ -33,4 +41,4 @@ public:
 
 }
 
-#endif // GLU_AST_EXPR_STRUCT_MEMBER_EXPR_KIND_HPP
+#endif // GLU_AST_EXPR_STRUCT_MEMBER_EXPR_HPP
