61585997e59dde20e9761d4fa7983b852682496c
yunse <yunse@UNOMBA15.local>
2025-03-01 00:01:04 +0900

    feat: basic of SextInst and ZextInst

---
+++ include/GIL/Instructions/IntZextInst.hpp
@@ -0,0 +1,27 @@
+#ifndef GLU_GIL_INSTRUCTIONS_INT_ZEXT_INST_HPP
+#define GLU_GIL_INSTRUCTIONS_INT_ZEXT_INST_HPP
+
+#include "ConversionInst.hpp"
+
+namespace glu::gil {
+
+/// @class intZextInst
+/// @brief Represents an instruction to zero-extend an integer value.
+///
+/// This class is derived from ConversionInst and represents an instruction
+/// to zero-extend an integer value in the GLU GIL (Generic Intermediate Language).
+class intZextInst : public ConversionInst {
+public:
+    /// @brief Constructs an intZextInst object.
+    ///
+    /// @param type The target type after zero extension.
+    /// @param value The integer value to be zero-extended.
+    intZextInst(Type type, Value value)
+        : ConversionInst(InstKind::intZextInstKind, type, value)
+    {
+    }
+};
+
+} // end namespace glu::gil
+
+#endif // GLU_GIL_INSTRUCTIONS_INT_ZEXT_INST_HPP
