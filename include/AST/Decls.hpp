893ac14eadd59957a4dbc6e82bad6e94fe75eeed
Marouan Bader <marouan.bader@epitech.eu>
2025-03-09 20:37:06 +0900

    feat: add AssignTmt test

--- include/AST/Decls.hpp
+++ include/AST/Decls.hpp
@@ -2,11 +2,13 @@
 #define GLU_AST_DECLS_HPP
 
 #include "Decl/EnumDecl.hpp"
+#include "Decl/ForBindingDecl.hpp"
 #include "Decl/FunctionDecl.hpp"
 #include "Decl/ImportDecl.hpp"
 #include "Decl/LetDecl.hpp"
 #include "Decl/StructDecl.hpp"
 #include "Decl/TypeAliasDecl.hpp"
 #include "Decl/VarDecl.hpp"
+#include "Decl/VarLetDecl.hpp"
 
 #endif // GLU_AST_DECLS_HPP
