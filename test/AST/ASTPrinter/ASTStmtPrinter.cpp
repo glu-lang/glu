9120cc68bb1eee03346338431d20d9954f5b1ef2
Marouan Bader <marouan.bader@epitech.eu>
2025-03-09 20:37:06 +0900

    feat: add AssignTmt test

--- test/AST/ASTPrinter/ASTStmtPrinter.cpp
+++ test/AST/ASTPrinter/ASTStmtPrinter.cpp
@@ -1,47 +1,48 @@
-#include "AST/ASTNode.hpp"
-#include "AST/Decls.hpp"
-#include "AST/Exprs.hpp"
-#include "AST/Stmts.hpp"
-#include "ASTPrinter.hpp"
+#include "AST/ASTContext.hpp"
+#include "AST/ASTPrinter.hpp"
+#include "Basic/SourceManager.hpp"
+#include "Basic/Tokens.hpp"
+#include "Lexer/Scanner.hpp"
+#include "llvm/Support/MemoryBuffer.h"
 #include <gtest/gtest.h>
 #include <llvm/Support/raw_ostream.h>
-
-namespace glu::ast {
 
 class ASTPrinterTest : public ::testing::Test {
 protected:
     std::string str;
-    llvm::raw_string_ostream os;
-    ASTPrinter printer;
-    SourceManager srcManager;
+    glu::SourceManager sm;
+    glu::ast::ASTContext ctx;
+    llvm::raw_string_ostream out;
+    glu::TypedMemoryArena<glu::ast::ASTNode> &ast;
 
-    ASTPrinterTest() : os(str), printer(&srcManager, os)
-    {
-        if (!srcManager.loadFile("test/AST/ASTPrinter/Samples/Nodes.glu"))
-            llvm::errs() << "Error loading file\n";
-    }
+    ASTPrinterTest() : out(str), ast(ctx.getASTMemoryArena()) { }
 };
 
-class TestNode : public ASTNode {
-public:
-    TestNode(NodeKind kind, glu::SourceLocation loc, TestNode *parent = nullptr)
-        : ASTNode(kind, loc, parent)
-    {
-    }
-};
-
-TEST_F(ASTPrinterTest, PrintNullASTNode)
+TEST_F(ASTPrinterTest, PrintAssignStmt)
 {
-    printer.visit(nullptr);
-    EXPECT_EQ(str, "Null ASTNode\n");
-}
+    std::string stmt = "x = 42";
+    std::unique_ptr<llvm::MemoryBuffer> buf(
+        llvm::MemoryBuffer::getMemBufferCopy(stmt)
+    );
+    sm.loadBuffer(std::move(buf), "test.glu");
 
-TEST_F(ASTPrinterTest, PrintSimpleASTNode)
-{
-    BinaryOpExpr binNode(SourceLocation(1));
-    ExpressionStmt exprNode(SourceLocation(2), &binNode);
-    printer.visit(&exprNode);
-    EXPECT_EQ(str, "ExpressionStmt at loc : 42\n  BinaryOpExpr at loc : 10\n");
-}
+    glu::Token assignToken(glu::TokenKind::equalTok, "=");
+    auto lhs = ast.create<glu::ast::RefExpr>(glu::SourceLocation(1), "x");
+    glu::types::IntTy intTy(glu::types::IntTy::Signed, 32);
+    auto rhs = ast.create<glu::ast::LiteralExpr>(
+        llvm::APInt(32, 42), &intTy, glu::SourceLocation(3)
+    );
+    glu::ast::AssignStmt node(glu::SourceLocation(0), lhs, assignToken, rhs);
 
-} // namespace glu::ast
+    node.print(&sm, out);
+
+    EXPECT_EQ(
+        out.str(),
+        "AssignStmt at file : test.glu line : 1 col : 1\n"
+        "  -->equal assignement with: \n"
+        "  RefExpr at file : test.glu line : 1 col : 2\n"
+        "    -->Reference to: x\n"
+        "  LiteralExpr at file : test.glu line : 1 col : 4\n"
+        "    -->Integer: 42\n"
+    );
+}
