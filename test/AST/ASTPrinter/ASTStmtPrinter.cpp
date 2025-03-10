f070e73469789760087cc2f3ac3ba40354eef16e
Marouan Bader <marouan.bader@epitech.eu>
2025-03-11 01:49:26 +0900

    chore: adapt RefExpr creation to new arena

--- test/AST/ASTPrinter/ASTStmtPrinter.cpp
+++ test/AST/ASTPrinter/ASTStmtPrinter.cpp
@@ -6,7 +6,11 @@ TEST_F(ASTPrinterTest, PrintAssignStmt)
     PREP_ASTPRINTER("x = 42", "AssignStmt.glu");
 
     glu::Token assignToken(glu::TokenKind::equalTok, "=");
-    auto lhs = ast.create<glu::ast::RefExpr>(glu::SourceLocation(0), "x");
+    auto lhs = ctx.getASTMemoryArena().create<glu::ast::RefExpr>(
+        glu::SourceLocation(0),
+        glu::ast::NamespaceIdentifier { std::array { llvm::StringRef("x") },
+                                        "x" }
+    );
     glu::types::IntTy intTy(glu::types::IntTy::Signed, 32);
     auto rhs = ast.create<glu::ast::LiteralExpr>(
         llvm::APInt(32, 42), &intTy, glu::SourceLocation(4)
