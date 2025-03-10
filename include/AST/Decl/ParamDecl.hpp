2728974d475b88c14c62f094eb0552c00d1cb1c7
Jeremy Elalouf <jeremy.elalouf@epitech.eu>
2025-03-10 09:45:38 +0900

    feat: `VarLetDecl` constructor take an `llvm::StringRef` instead to avoid any leaks in our AST

--- include/AST/Decl/ParamDecl.hpp
+++ include/AST/Decl/ParamDecl.hpp
@@ -18,12 +18,10 @@ public:
     /// @param type The type of the declared param.
     /// @param value The value assigned to the declared param.
     ParamDecl(
-        SourceLocation location, std::string name, glu::types::TypeBase *type,
-        ExprBase *value
+        SourceLocation location, llvm::StringRef name,
+        glu::types::TypeBase *type, ExprBase *value
     )
-        : VarLetDecl(
-              NodeKind::ParamDeclKind, location, std::move(name), type, value
-          )
+        : VarLetDecl(NodeKind::ParamDeclKind, location, name, type, value)
     {
     }
 
