833ce1b3bbfacb7443f4dcac4361e43423f65bfb
Marouan Bader <marouan.bader@epitech.eu>
2025-03-19 17:33:34 +0900

    feature: add generic implementation of detailed nodes, tests and impl for every stmts and expr

--- test/AST/ASTPrinter/ASTExprPrinter.cpp
+++ test/AST/ASTPrinter/ASTExprPrinter.cpp
@@ -25,10 +25,12 @@ TEST_F(ASTPrinterTest, PrintBinaryOp)
                 "-->plusOp Binary Operation with:\n"
                 "  -->LeftOperand:\n"
                 "    LiteralExpr "
-             << lhs << " <line:1:1> -->Integer: 1\n"
+             << lhs << " <line:1:1>\n"
+             << "      -->Integer: 1\n"
              << "  -->RightOperand:\n"
                 "    LiteralExpr "
-             << rhs << " <line:1:5> -->Integer: 2\n";
+             << rhs << " <line:1:5>\n"
+             << "      -->Integer: 2\n";
 
     EXPECT_EQ(os.str(), expected.str());
 }
@@ -66,13 +68,17 @@ TEST_F(ASTPrinterTest, PrintCallExpr)
                 "  -->Callee:\n"
                 "    RefExpr "
              << callee
-             << " <line:1:1> -->Reference to: foo\n"
+             << " <line:1:1>\n"
+                "      -->Reference to: foo\n"
                 "  -->Args:\n"
                 "    LiteralExpr "
              << arg1
-             << " <line:1:5> -->Integer: 42\n"
+             << " <line:1:5>\n"
+                "      -->Integer: 42\n"
                 "    RefExpr "
-             << arg2 << " <line:1:9> -->Reference to: x\n";
+             << arg2
+             << " <line:1:9>\n"
+                "      -->Reference to: x\n";
 
     EXPECT_EQ(os.str(), expected.str());
 }
@@ -97,9 +103,9 @@ TEST_F(ASTPrinterTest, PrintCastExpr)
     expected << "CastExpr " << node << " <CastExpr.glu, line:1:1>\n"
              << "-->Casting to Int:\n"
              << "  -->CastedExpr:\n"
-             << "    RefExpr " << value << " <line:1:11> -->Reference to: x\n";
+             << "    RefExpr " << value << " <line:1:11>\n"
+             << "      -->Reference to: x\n";
 
-    llvm::outs() << os.str() << "\n";
     EXPECT_EQ(os.str(), expected.str());
 }
 
@@ -115,8 +121,144 @@ TEST_F(ASTPrinterTest, PrintLiteralExpr)
     node->debugPrint(&sm, os);
 
     std::ostringstream expected;
-    expected << "LiteralExpr " << node
-             << " <LiteralExpr.glu, line:1:1> -->Integer: 42\n";
-    llvm::outs() << os.str() << "\n";
+    expected << "LiteralExpr " << node << " <LiteralExpr.glu, line:1:1>\n"
+             << "  -->Integer: 42\n";
+    EXPECT_EQ(os.str(), expected.str());
+}
+
+TEST_F(ASTPrinterTest, PrintStructMemberExpr)
+{
+    PREP_ASTPRINTER("myStruct.member", "StructMemberExpr.glu");
+
+    auto base = ast.create<glu::ast::RefExpr>(
+        glu::SourceLocation(0),
+        glu::ast::NamespaceIdentifier {
+            std::array { llvm::StringRef("myStruct") }, "myStruct" }
+    );
+
+    auto node = ast.create<glu::ast::StructMemberExpr>(
+        glu::SourceLocation(9), base, "member"
+    );
+
+    node->debugPrint(&sm, os);
+
+    std::ostringstream expected;
+    expected << "StructMemberExpr " << node
+             << " <StructMemberExpr.glu, line:1:10>\n"
+                "-->Member: member from struct:\n"
+                "  -->StructExpr:\n"
+                "    RefExpr "
+             << base
+             << " <line:1:1>\n"
+                "      -->Reference to: myStruct\n";
+
+    EXPECT_EQ(os.str(), expected.str());
+}
+
+TEST_F(ASTPrinterTest, PrintUnaryOpExpr)
+{
+    PREP_ASTPRINTER("-x", "UnaryOpExpr.glu");
+
+    auto operand = ast.create<glu::ast::RefExpr>(
+        glu::SourceLocation(1),
+        glu::ast::NamespaceIdentifier { std::array { llvm::StringRef("x") },
+                                        "x" }
+    );
+
+    auto node = ast.create<glu::ast::UnaryOpExpr>(
+        glu::SourceLocation(0), operand,
+        glu::Token(glu::TokenKind::subOpTok, "-")
+    );
+
+    node->debugPrint(&sm, os);
+
+    std::ostringstream expected;
+    expected << "UnaryOpExpr " << node
+             << " <UnaryOpExpr.glu, line:1:1>\n"
+                "-->subOp Unary Operation with:\n"
+                "  -->Operand:\n"
+                "    RefExpr "
+             << operand
+             << " <line:1:2>\n"
+                "      -->Reference to: x\n";
+
+    EXPECT_EQ(os.str(), expected.str());
+}
+
+TEST_F(ASTPrinterTest, PrintTernaryConditionalExpr)
+{
+    PREP_ASTPRINTER("x > 0 ? x : -x", "TernaryConditionalExpr.glu");
+
+    glu::types::IntTy intTy(glu::types::IntTy::Signed, 32);
+
+    auto condition = ast.create<glu::ast::BinaryOpExpr>(
+        glu::SourceLocation(0),
+        ast.create<glu::ast::RefExpr>(
+            glu::SourceLocation(0),
+            glu::ast::NamespaceIdentifier { std::array { llvm::StringRef("x") },
+                                            "x" }
+        ),
+        glu::Token(glu::TokenKind::gtOpTok, ">"),
+        ast.create<glu::ast::LiteralExpr>(
+            llvm::APInt(32, 0), &intTy, glu::SourceLocation(4)
+        )
+    );
+
+    auto trueExpr = ast.create<glu::ast::RefExpr>(
+        glu::SourceLocation(8),
+        glu::ast::NamespaceIdentifier { std::array { llvm::StringRef("x") },
+                                        "x" }
+    );
+
+    auto falseExpr = ast.create<glu::ast::UnaryOpExpr>(
+        glu::SourceLocation(12),
+        ast.create<glu::ast::RefExpr>(
+            glu::SourceLocation(13),
+            glu::ast::NamespaceIdentifier { std::array { llvm::StringRef("x") },
+                                            "x" }
+        ),
+        glu::Token(glu::TokenKind::subOpTok, "-")
+    );
+
+    auto node = ast.create<glu::ast::TernaryConditionalExpr>(
+        glu::SourceLocation(0), condition, trueExpr, falseExpr
+    );
+
+    node->debugPrint(&sm, os);
+
+    std::ostringstream expected;
+    expected << "TernaryConditionalExpr " << node
+             << " <TernaryConditionalExpr.glu, line:1:1>\n"
+                "  -->Condition:\n"
+                "    BinaryOpExpr "
+             << condition
+             << " <line:1:1>\n"
+                "    -->gtOp Binary Operation with:\n"
+                "      -->LeftOperand:\n"
+                "        RefExpr "
+             << condition->getLeftOperand()
+             << " <line:1:1>\n"
+                "          -->Reference to: x\n"
+                "      -->RightOperand:\n"
+                "        LiteralExpr "
+             << condition->getRightOperand()
+             << " <line:1:5>\n"
+                "          -->Integer: 0\n"
+                "  -->TrueExpr:\n"
+                "    RefExpr "
+             << trueExpr
+             << " <line:1:9>\n"
+                "      -->Reference to: x\n"
+                "  -->FalseExpr:\n"
+                "    UnaryOpExpr "
+             << falseExpr
+             << " <line:1:13>\n"
+                "    -->subOp Unary Operation with:\n"
+                "      -->Operand:\n"
+                "        RefExpr "
+             << falseExpr->getOperand()
+             << " <line:1:14>\n"
+                "          -->Reference to: x\n";
+
     EXPECT_EQ(os.str(), expected.str());
 }
