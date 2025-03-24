67d6a7b73967818dda8e7dd0c183b82faf3810c3
Marouan Bader <marouan.bader@epitech.eu>
2025-03-24 17:16:11 +0900

    feat: add structFieldPtrInst class

--- include/GIL/Instructions/StructFieldPtrInst.hpp
+++ include/GIL/Instructions/StructFieldPtrInst.hpp
@@ -1,4 +1,53 @@
 #ifndef GLU_GIL_INSTRUCTIONS_STRUCT_FIELD_PTR_INST_HPP
 #define GLU_GIL_INSTRUCTIONS_STRUCT_FIELD_PTR_INST_HPP
 
+#include "InstBase.hpp"
+
+namespace glu::gil {
+
+class StructFieldPtrInst : public InstBase {
+    Value _structValue;
+    Member _member;
+    Type _ptr;
+
+public:
+    StructFieldPtrInst(Value structValue, Member member)
+        : InstBase(InstKind::StructFieldPtrInstKind)
+        , _structValue(structValue)
+        , _member(member)
+    {
+    }
+
+    Value getStructValue() const { return _structValue; }
+
+    Member getMember() const { return _member; }
+
+    virtual size_t getResultCount() const override { return 1; }
+
+    virtual size_t getOperandCount() const override { return 2; }
+
+    virtual Operand getOperand(size_t index) const override
+    {
+        switch (index) {
+        case 0: return _structValue;
+        case 1: return _member;
+        default: llvm_unreachable("Invalid operand index");
+        }
+    }
+
+    // TODO: Implement Member class to create PointerTy from member type
+    // virtual Type getResultType(size_t index) const override
+    // {
+    //     assert(index == 0 && "Result index out of range");
+    //     return glu::types::PointerTy member.getType();
+    // }
+
+    static bool classof(InstBase const *inst)
+    {
+        return inst->getKind() == InstKind::StructFieldPtrInstKind;
+    }
+};
+
+} // namespace glu::gil
+
 #endif GLU_GIL_INSTRUCTIONS_INST_BASE_HPP
