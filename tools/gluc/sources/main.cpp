fa26b6d5d00a4e211591700d1ee5a24dc376c351
LindonAliu <lindon.aliu@outlook.fr>
2025-03-29 00:16:59 +0900

    feat(gluc): add options to print the AST generated for many files

--- tools/gluc/sources/main.cpp
+++ tools/gluc/sources/main.cpp
@@ -1,30 +1,50 @@
 #include "Basic/SourceLocation.hpp"
 #include "Basic/SourceManager.hpp"
-#include "IRGen/IRGen.hpp"
 #include "Lexer/Scanner.hpp"
 #include "Parser/Parser.hpp"
 
-int main(int argc, char *argv[])
+#include "llvm/Support/CommandLine.h"
+
+using namespace llvm;
+
+static cl::opt<bool> PrintAST(
+    "print-ast", cl::desc("Print the AST after parsing"), cl::init(false)
+);
+
+static cl::list<std::string> InputFilenames(
+    cl::Positional, cl::OneOrMore, cl::desc("<input glu files>")
+);
+
+int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        llvm::errs() << "Usage: " << argv[0] << " <input file>\n";
+    cl::ParseCommandLineOptions(argc, argv);
+
+    if (InputFilenames.empty()) {
+        errs() << "Usage: " << argv[0] << " <input files...>\n";
         return 1;
     }
 
     glu::SourceManager sourceManager;
-
-    auto fileID = sourceManager.loadFile(argv[1]);
-
-    if (!fileID) {
-        llvm::errs() << fileID.getError().message() << "\n";
-        return 1;
-    }
-
-    glu::Scanner scanner(sourceManager.getBuffer(*fileID));
     glu::ast::ASTContext context;
-    glu::Parser parser(scanner, context, sourceManager);
 
-    if (parser.parse()) {
-        parser.getAST()->debugPrint();
+    for (auto const &inputFile : InputFilenames) {
+        auto fileID = sourceManager.loadFile(inputFile.c_str());
+
+        if (!fileID) {
+            errs() << "Error loading " << inputFile << ": "
+                   << fileID.getError().message() << "\n";
+            continue;
+        }
+
+        glu::Scanner scanner(sourceManager.getBuffer(*fileID));
+        glu::Parser parser(scanner, context, sourceManager);
+
+        if (parser.parse()) {
+            if (PrintAST) {
+                parser.getAST()->debugPrint();
+            }
+        }
     }
+
+    return 0;
 }
