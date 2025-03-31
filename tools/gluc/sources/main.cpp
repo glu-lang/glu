800c9f7baea5a375bd1defce31cae7d43322ea7d
LindonAliu <lindon.aliu@outlook.fr>
2025-03-31 11:33:29 +0900

    feat(driver): add an option to print GIL

--- tools/gluc/sources/main.cpp
+++ tools/gluc/sources/main.cpp
@@ -1,5 +1,7 @@
 #include "Basic/SourceLocation.hpp"
 #include "Basic/SourceManager.hpp"
+#include "GIL/GILPrinter.hpp"
+#include "GILGen/GILGen.hpp"
 #include "Lexer/Scanner.hpp"
 #include "Parser/Parser.hpp"
 
@@ -16,6 +18,10 @@ static cl::opt<bool> PrintTokens(
     cl::init(false)
 );
 
+static cl::opt<bool> PrintGIL(
+    "print-gil", cl::desc("Print GIL after generation"), cl::init(false)
+);
+
 static cl::list<std::string> InputFilenames(
     cl::Positional, cl::OneOrMore, cl::desc("<input glu files>")
 );
@@ -26,6 +32,8 @@ int main(int argc, char **argv)
 
     glu::SourceManager sourceManager;
     glu::ast::ASTContext context;
+    llvm::BumpPtrAllocator GILFuncArena;
+    glu::gil::GILPrinter GILPrinter(sourceManager);
 
     for (auto const &inputFile : InputFilenames) {
         auto fileID = sourceManager.loadFile(inputFile.c_str());
@@ -40,9 +48,29 @@ int main(int argc, char **argv)
         glu::Parser parser(scanner, context, sourceManager, PrintTokens);
 
         if (parser.parse()) {
-            if (PrintAST) {
-                parser.getAST()->debugPrint();
+            auto ast = llvm::cast<glu::ast::ModuleDecl>(parser.getAST());
+
+            if (!ast) {
+                continue;
             }
+
+            if (PrintAST) {
+                ast->debugPrint();
+            }
+
+            for (auto decl : ast->getDecls())
+                if (auto fn = llvm::dyn_cast<glu::ast::FunctionDecl>(decl)) {
+                    glu::gil::Function *GILFn
+                        = glu::gilgen::GILGen().generateFunction(
+                            fn, GILFuncArena
+                        );
+
+                    if (PrintGIL) {
+                        GILPrinter.visit(GILFn);
+                    }
+                }
+        } else {
+            std::cerr << "Error parsing" << std::endl;
         }
     }
 
