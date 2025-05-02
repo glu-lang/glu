204b947fb6aadf6313eb7ecb5f124392db7bb8f6
Marouan Bader <marouan.bader@epitech.eu>
2025-05-02 13:31:39 +0900

    chore: rename Constraints file into Constraint

---
+++ include/Sema/ConstraintSystem.hpp
@@ -0,0 +1,14 @@
+#ifndef GLU_SEMA_CONSTRAINT_SYSTEM_HPP
+#define GLU_SEMA_CONSTRAINT_SYSTEM_HPP
+
+#include "Constraints.hpp"
+
+namespace glu::sema {
+
+class ConstraintSystem {
+
+}
+
+}
+
+#endif // GLU_SEMA_CONSTRAINT_SYSTEM_HPP
