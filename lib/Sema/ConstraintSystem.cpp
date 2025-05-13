a09ad2f96ede4c1201cf662deaf648dda7294540
Marouan Bader <marouan.bader@epitech.eu>
2025-05-13 17:26:36 +0900

    feat: enhance TypeVariableTy and ConstraintSystem with constructors and methods

---
+++ lib/Sema/ConstraintSystem.cpp
@@ -0,0 +1,32 @@
+#include "ConstraintSystem.hpp"
+
+namespace glu::sema {
+
+ConstraintSystem::ConstraintSystem(
+    ScopeTable *scopeTable
+)
+    : _scopeTable(scopeTable)
+    , _typeVariables()
+    , _allocator()
+    , _constraints()
+    , _bestSolutions()
+{
+}
+
+Constraint *ConstraintSystem::getBestSolution(Constraint *constraint)
+{
+    auto it = _bestSolutions.find(constraint);
+    if (it != _bestSolutions.end())
+        return it->second.second;
+    return nullptr;
+}
+
+unsigned ConstraintSystem::getBestSolutionScore(Constraint *constraint)
+{
+    auto it = _bestSolutions.find(constraint);
+    if (it != _bestSolutions.end())
+        return it->second.first;
+    return 0;
+}
+
+}
