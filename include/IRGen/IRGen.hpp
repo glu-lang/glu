e690965caf10682cafecc8e67715c0a62a39c05c
Emil Pedersen <emil.pedersen@epitech.eu>
2025-07-08 18:03:21 +0200

    feat(irgen): basic irgen implementation for void unreachable

--- include/IRGen/IRGen.hpp
+++ include/IRGen/IRGen.hpp
@@ -1,5 +1,19 @@
+#ifndef GLU_IRGEN_IRGEN_HPP
+#define GLU_IRGEN_IRGEN_HPP
+
+#include "GIL/Module.hpp"
+
+#include <llvm/IR/Module.h>
+
+namespace glu::irgen {
+
+/// @brief IRGen is the main class for generating intermediate representation
+/// (IR) from GIL instructions.
 class IRGen {
 public:
-    IRGen();
-    ~IRGen() = default;
+    void generateIR(llvm::Module &out, glu::gil::Module *mod);
 };
+
+} // namespace glu::irgen
+
+#endif // GLU_IRGEN_IRGEN_HPP
--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -1,3 +1,77 @@
 #include "IRGen.hpp"
 
-IRGen::IRGen() { }
+#include "GIL/InstVisitor.hpp"
+
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+
+namespace glu::irgen {
+
+/// @brief IRGenImpl is the implementation of the IRGen visitor.
+/// It inherits from glu::gil::InstVisitor and provides the necessary methods
+/// to visit different instruction types in the GIL intermediate representation.
+/// This class is used to generate LLVM IR from GIL instructions.
+struct IRGenVisitor : public glu::gil::InstVisitor<IRGenVisitor> {
+    llvm::Module &outModule;
+    llvm::LLVMContext &ctx;
+    llvm::IRBuilder<> builder;
+
+    // State
+    llvm::Function *f = nullptr;
+    llvm::BasicBlock *bb = nullptr;
+
+    IRGenVisitor(llvm::Module &module)
+        : outModule(module), ctx(module.getContext()), builder(ctx)
+    {
+    }
+
+    void beforeVisitFunction(glu::gil::Function *fn)
+    {
+        assert(!f && "Callbacks should be called in the right order");
+        // Convert GIL function to LLVM function
+        // TODO: Convert GIL function type to LLVM function type
+        llvm::FunctionType *funcType
+            = llvm::FunctionType::get(llvm::Type::getVoidTy(ctx), false);
+        f = llvm::Function::Create(
+            funcType, llvm::Function::ExternalLinkage, fn->getName(), outModule
+        );
+    }
+
+    void afterVisitFunction([[maybe_unused]] glu::gil::Function *fn)
+    {
+        assert(f && "Callbacks should be called in the right order");
+        // Verify the function (optional, good for debugging the compiler)
+        llvm::verifyFunction(*f);
+        f = nullptr;
+    }
+
+    void beforeVisitBasicBlock(glu::gil::BasicBlock *block)
+    {
+        assert(!bb && "Callbacks should be called in the right order");
+        // Create a new LLVM basic block
+        bb = llvm::BasicBlock::Create(ctx, block->getLabel(), f);
+        // TODO: phi nodes for arguments
+        builder.SetInsertPoint(bb);
+    }
+
+    void afterVisitBasicBlock([[maybe_unused]] glu::gil::BasicBlock *block)
+    {
+        assert(bb && "Callbacks should be called in the right order");
+        bb = nullptr;
+    }
+
+    void visitUnreachableInst([[maybe_unused]] glu::gil::UnreachableInst *inst)
+    {
+        builder.CreateUnreachable();
+    }
+};
+
+void IRGen::generateIR(llvm::Module &out, glu::gil::Module *mod)
+{
+    IRGenVisitor visitor(out);
+    // Visit the module to generate IR
+    visitor.visit(mod);
+}
+
+} // namespace glu::irgen
