8490f4c766edcde519ee0f653df87a4b1978d94c
Marouan Bader <marouan.bader@epitech.eu>
2025-07-08 18:11:43 +0200

    style: lint TypeMapper.hpp

--- include/Sema/TypeMapper.hpp
+++ include/Sema/TypeMapper.hpp
@@ -6,15 +6,15 @@ template <typename Impl>
 class TypeMapper : public glu::ast::ASTWalker<TypeMapper<Impl>, void> {
     Impl asImpl() { return static_cast<Impl &>(*this); }
 
-#define NODE_KIND_(Name, Parent, ...)                       \
-    NODE_KIND_SUPER(NodeName, Parent)                       \
-    NODE_TYPEREF(Type, TypeName)                            \
-    void _visit##NodeName(NodeName *node) { \
-        this->asImpl()->mapType##Type(node->get##TypeName)  \
+#define NODE_KIND_(Name, Parent, ...)                      \
+    NODE_KIND_SUPER(NodeName, Parent)                      \
+    NODE_TYPEREF(Type, TypeName)                           \
+    void _visit##NodeName(NodeName *node) {                \
+        this->asImpl()->mapType##Type(node->get##TypeName) \
     }
 #define NODE_KIND(Name, Parent)
 
-#define NODE_TYPEREF(Type, Name)                                    \
+#define NODE_TYPEREF(Type, Name)                    \
     void mapType##Name(Name *type) { return type; }
 };
 }
