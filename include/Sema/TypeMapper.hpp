b43a007552e5dd7dbe74f187e7bb477eccf20af9
Marouan Bader <marouan.bader@epitech.eu>
2025-07-11 15:31:32 +0200

    style: lint TypeMapper.hpp

--- include/Sema/TypeMapper.hpp
+++ include/Sema/TypeMapper.hpp
@@ -6,13 +6,17 @@ namespace glu::sema {
 template <typename Impl>
 class TypeMapper : public glu::ast::ASTWalker<Impl, void> {
 public:
-#define NODE_KIND_(NodeName, Parent, ...)                                      \
-    void postVisit##NodeName([[maybe_unused]] ast::NodeName *node) { __VA_ARGS__; }
+#define NODE_KIND_(NodeName, Parent, ...)                          \
+    void postVisit##NodeName([[maybe_unused]] ast::NodeName *node) \
+    {                                                              \
+        __VA_ARGS__;                                               \
+    }
 #define NODE_KIND(Name, Parent)
 
-#define NODE_TYPEREF(Type, Name)                                            \
-    node->set##Name(                                                        \
-        llvm::cast<glu::types::Type>(this->asImpl()->mapType(node->get##Name())) \
+#define NODE_TYPEREF(Type, Name)                                               \
+    node->set##Name(                                                           \
+        llvm::cast<glu::types::Type>(this->asImpl()->mapType(node->get##Name() \
+        ))                                                                     \
     )
 
 #define NODE_CHILD(Type, Name) (void) 0
