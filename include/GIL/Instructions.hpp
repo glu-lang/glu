693d709f66c6f86900f01808b8018751f0f0d1e7
LindonAliu <lindon.aliu@outlook.fr>
2025-10-01 17:58:40 +0200

    style: reorder OSSA instruction includes for better organization

--- include/GIL/Instructions.hpp
+++ include/GIL/Instructions.hpp
@@ -32,13 +32,13 @@
 #include "Instructions/UnreachableInst.hpp"
 
 // OSSA Instructions
-#include "Instructions/OSSAInst.hpp"
-#include "Instructions/MoveInst.hpp"
 #include "Instructions/CopyInst.hpp"
 #include "Instructions/DropInst.hpp"
-#include "Instructions/MutableBorrowInst.hpp"
-#include "Instructions/ImmutableBorrowInst.hpp"
 #include "Instructions/EndBorrowInst.hpp"
+#include "Instructions/ImmutableBorrowInst.hpp"
+#include "Instructions/MoveInst.hpp"
+#include "Instructions/MutableBorrowInst.hpp"
+#include "Instructions/OSSAInst.hpp"
 
 // Needed by InstBase
 #include "BasicBlock.hpp"
