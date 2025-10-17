da587fee32a690ba2df24f1ceed0c9a5c5e3725c
Marouan Bader <marouan.bader@epitech.eu>
2025-10-17 10:29:44 +0200

    feat: add unit tests for DITypeLifter

---
+++ test/IRDec/DITypeLifterTest.cpp
@@ -0,0 +1,79 @@
+#include "IRDec/DITypeLifter.hpp"
+#include "AST/ASTContext.hpp"
+
+#include <llvm/IR/DIBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+
+#include <gtest/gtest.h>
+#include <llvm/Support/Casting.h>
+
+using namespace glu;
+
+class DITypeLifterTest : public ::testing::Test {
+protected:
+    llvm::LLVMContext ctx;
+    llvm::Module module { "test", ctx };
+    glu::ast::ASTContext astCtx;
+
+    DITypeLifterTest() : astCtx() { }
+};
+
+TEST_F(DITypeLifterTest, LiftSignedIntBasicType)
+{
+    llvm::DIBuilder dib(module);
+    auto *file = dib.createFile("t", ".");
+    auto *compileUnit = dib.createCompileUnit(
+        llvm::dwarf::DW_LANG_C, file, "test", false, "", 0
+    );
+
+    // create a signed 32-bit basic type
+    auto *diInt = dib.createBasicType("int32", 32, llvm::dwarf::DW_ATE_signed);
+
+    dib.finalize();
+
+    irdec::DITypeLifter lifter(astCtx);
+    auto *lifted = lifter.lift(diInt);
+    ASSERT_NE(lifted, nullptr);
+    auto *intTy = llvm::dyn_cast<types::IntTy>(lifted);
+    ASSERT_NE(intTy, nullptr);
+    EXPECT_EQ(intTy->getSignedness(), types::IntTy::Signed);
+    EXPECT_EQ(intTy->getBitWidth(), 32);
+}
+
+TEST_F(DITypeLifterTest, LiftUnsignedIntBasicType)
+{
+    llvm::DIBuilder dib(module);
+    auto *file = dib.createFile("t", ".");
+    dib.createCompileUnit(llvm::dwarf::DW_LANG_C, file, "test", false, "", 0);
+
+    auto *diUInt
+        = dib.createBasicType("uint16", 16, llvm::dwarf::DW_ATE_unsigned);
+    dib.finalize();
+
+    irdec::DITypeLifter lifter(astCtx);
+    auto *lifted = lifter.lift(diUInt);
+    ASSERT_NE(lifted, nullptr);
+    auto *intTy = llvm::dyn_cast<types::IntTy>(lifted);
+    ASSERT_NE(intTy, nullptr);
+    EXPECT_EQ(intTy->getSignedness(), types::IntTy::Unsigned);
+    EXPECT_EQ(intTy->getBitWidth(), 16);
+}
+
+TEST_F(DITypeLifterTest, LiftFloatBasicType)
+{
+    llvm::DIBuilder dib(module);
+    auto *file = dib.createFile("t", ".");
+    dib.createCompileUnit(llvm::dwarf::DW_LANG_C, file, "test", false, "", 0);
+
+    auto *diFloat
+        = dib.createBasicType("float32", 32, llvm::dwarf::DW_ATE_float);
+    dib.finalize();
+
+    irdec::DITypeLifter lifter(astCtx);
+    auto *lifted = lifter.lift(diFloat);
+    ASSERT_NE(lifted, nullptr);
+    auto *floatTy = llvm::dyn_cast<types::FloatTy>(lifted);
+    ASSERT_NE(floatTy, nullptr);
+    EXPECT_EQ(floatTy->getBitWidth(), 32);
+}
