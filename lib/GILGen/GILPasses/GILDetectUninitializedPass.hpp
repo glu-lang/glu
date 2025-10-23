3e84925c1084c11a02346fab2836358e929780e4
Yunse LEE <yunsecode@gmail.com>
2025-10-23 14:13:27 +0200

    chore: basic DFS for use unitializedPAss

--- lib/GILGen/GILPasses/GILDetectUninitializedPass.hpp
+++ lib/GILGen/GILPasses/GILDetectUninitializedPass.hpp
@@ -11,6 +11,11 @@
 #include <llvm/ADT/DenseSet.h>
 #include <llvm/ADT/SmallVector.h>
 
+namespace glu::gil {
+class BrInst;
+class CondBrInst;
+}
+
 namespace glu::gilgen {
 
 class GILDetectUninitializedPass
@@ -18,11 +23,85 @@ class GILDetectUninitializedPass
 private:
     DiagnosticManager &diagManager;
 
+    llvm::DenseMap<gil::BasicBlock *, llvm::SmallVector<gil::BasicBlock *, 4>>
+        predecessorMap;
+
+    void buildPredecessorMap(gil::Function *func)
+    {
+        predecessorMap.clear();
+
+        llvm::DenseSet<gil::BasicBlock *> visited;
+        llvm::SmallVector<gil::BasicBlock *, 32> stack;
+
+        if (func->getBasicBlockCount() > 0) {
+            stack.push_back(func->getEntryBlock());
+        }
+
+        while (!stack.empty()) {
+            gil::BasicBlock *currentBB = stack.pop_back_val();
+
+            if (!visited.insert(currentBB).second) {
+                continue;
+            }
+
+            auto *terminator = currentBB->getTerminator();
+            if (!terminator)
+                continue;
+
+            llvm::SmallVector<gil::BasicBlock *, 4> successors;
+
+            if (auto *brInst = llvm::dyn_cast<gil::BrInst>(terminator)) {
+                if (auto *dest = brInst->getDestination()) {
+                    successors.push_back(dest);
+                }
+            } else if (auto *condBr
+                       = llvm::dyn_cast<gil::CondBrInst>(terminator)) {
+                if (auto *thenBlock = condBr->getThenBlock()) {
+                    successors.push_back(thenBlock);
+                }
+                if (auto *elseBlock = condBr->getElseBlock()) {
+                    successors.push_back(elseBlock);
+                }
+            }
+
+            for (auto *successor : successors) {
+                predecessorMap[successor].push_back(currentBB);
+
+                if (visited.find(successor) == visited.end()) {
+                    stack.push_back(successor);
+                }
+            }
+        }
+    }
+
+    llvm::ArrayRef<gil::BasicBlock *> getPredecessors(gil::BasicBlock *bb)
+    {
+        auto it = predecessorMap.find(bb);
+        if (it != predecessorMap.end()) {
+            return it->second;
+        }
+        return {};
+    }
+
 public:
     GILDetectUninitializedPass(DiagnosticManager &diagManager)
         : diagManager(diagManager)
     {
     }
+
+    void beforeVisitFunction(gil::Function *func)
+    {
+        buildPredecessorMap(func);
+
+        for (auto &bb : func->getBasicBlocks()) {
+            auto preds = getPredecessors(&bb);
+            if (!preds.empty()) { }
+        }
+    }
+
+    void beforeVisitBasicBlock(gil::BasicBlock *bb) { }
+
+    void afterVisitFunction(gil::Function *func) { predecessorMap.clear(); }
 };
 
 } // namespace glu::gilgen
