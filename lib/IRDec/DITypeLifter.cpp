830d25cc3337201529cc027f42f39b96d64a9630
Emil Pedersen <emil.pedersen@epitech.eu>
2025-11-17 00:05:26 +0100

    refactor(irdec): inline function definitions

--- lib/IRDec/DITypeLifter.cpp
+++ lib/IRDec/DITypeLifter.cpp
@@ -15,233 +15,225 @@ public:
         : _ctx(context), _context(context.ast)
     {
     }
-    ~DITypeLifter() = default;
 
     /// @brief Lift a DIType to a GLU type
     /// @param diType The DIType to lift
     /// @return The lifted GLU type, or nullptr if the type could not be lifted
-    glu::types::TypeBase *lift(llvm::DIType const *diType);
-
-    glu::types::TypeBase *
-    handleBasicType(llvm::DIBasicType const *diBasicType) const;
-
-    glu::types::TypeBase *
-    handleComposedTypes(llvm::DICompositeType const *diCompositeType);
-
-    glu::types::TypeBase *
-    handleDerivedType(llvm::DIDerivedType const *diDerivedType);
-
-    glu::types::TypeBase *
-    handleSubroutineType(llvm::DISubroutineType const *diSubroutineType);
-};
-
-glu::types::TypeBase *
-DITypeLifter::handleBasicType(llvm::DIBasicType const *diBasicType) const
-{
-    using Signedness = glu::types::IntTy::Signedness;
-
-    auto encoding = diBasicType->getEncoding();
-    auto sizeInBits = diBasicType->getSizeInBits();
-    auto &arena = _context.getTypesMemoryArena();
-
-    switch (encoding) {
-    case llvm::dwarf::DW_ATE_signed:
-        return arena.create<types::IntTy>(Signedness::Signed, sizeInBits);
-    case llvm::dwarf::DW_ATE_unsigned:
-        return arena.create<types::IntTy>(Signedness::Unsigned, sizeInBits);
-    case llvm::dwarf::DW_ATE_float:
-        return arena.create<types::FloatTy>(sizeInBits);
-    case llvm::dwarf::DW_ATE_boolean: return arena.create<types::BoolTy>();
-    case llvm::dwarf::DW_ATE_address:
-        return arena.create<types::PointerTy>(arena.create<types::CharTy>());
-    case llvm::dwarf::DW_ATE_unsigned_char:
-    case llvm::dwarf::DW_ATE_signed_char: return arena.create<types::CharTy>();
-    default: return nullptr;
-    }
-}
-
-glu::types::TypeBase *
-DITypeLifter::handleComposedTypes(llvm::DICompositeType const *diCompositeType)
-{
-    using namespace glu::types;
-
-    auto &astArena = _context.getASTMemoryArena();
-    auto &typesArena = _context.getTypesMemoryArena();
-    switch (diCompositeType->getTag()) {
-    case llvm::dwarf::DW_TAG_class_type:
-    case llvm::dwarf::DW_TAG_structure_type: {
-        if (auto *structDecl = llvm::dyn_cast_if_present<ast::StructDecl>(
-                _ctx._diTypeCache[diCompositeType]
-            )) {
-            return structDecl->getType();
+    glu::types::TypeBase *lift(llvm::DIType const *diType)
+    {
+        if (!diType) {
+            return _context.getTypesMemoryArena().create<types::VoidTy>();
         }
+        switch (diType->getMetadataID()) {
+        case llvm::Metadata::MetadataKind::DIBasicTypeKind:
+            return handleBasicType(llvm::cast<llvm::DIBasicType>(diType));
+        case llvm::Metadata::MetadataKind::DICompositeTypeKind:
+            return handleComposedTypes(
+                llvm::cast<llvm::DICompositeType>(diType)
+            );
+        case llvm::Metadata::MetadataKind::DIDerivedTypeKind:
+            return handleDerivedType(llvm::cast<llvm::DIDerivedType>(diType));
+        case llvm::Metadata::MetadataKind::DISubroutineTypeKind:
+            return handleSubroutineType(
+                llvm::cast<llvm::DISubroutineType>(diType)
+            );
+        default: return nullptr;
+        }
+    }
 
-        std::vector<ast::FieldDecl *> fieldDecls;
-        if (auto elements = diCompositeType->getElements()) {
-            for (auto *elem : elements) {
-                if (auto *derivedType
-                    = llvm::dyn_cast<llvm::DIDerivedType>(elem)) {
-                    if (derivedType->getTag() != llvm::dwarf::DW_TAG_member)
-                        continue;
-                    auto fieldName = derivedType->getName();
-                    auto fieldType = lift(derivedType->getBaseType());
-                    if (!fieldType) {
-                        return nullptr;
+    glu::types::TypeBase *
+    handleBasicType(llvm::DIBasicType const *diBasicType) const
+    {
+        using Signedness = glu::types::IntTy::Signedness;
+
+        auto encoding = diBasicType->getEncoding();
+        auto sizeInBits = diBasicType->getSizeInBits();
+        auto &arena = _context.getTypesMemoryArena();
+
+        switch (encoding) {
+        case llvm::dwarf::DW_ATE_signed:
+            return arena.create<types::IntTy>(Signedness::Signed, sizeInBits);
+        case llvm::dwarf::DW_ATE_unsigned:
+            return arena.create<types::IntTy>(Signedness::Unsigned, sizeInBits);
+        case llvm::dwarf::DW_ATE_float:
+            return arena.create<types::FloatTy>(sizeInBits);
+        case llvm::dwarf::DW_ATE_boolean: return arena.create<types::BoolTy>();
+        case llvm::dwarf::DW_ATE_address:
+            return arena.create<types::PointerTy>(
+                arena.create<types::CharTy>()
+            );
+        case llvm::dwarf::DW_ATE_unsigned_char:
+        case llvm::dwarf::DW_ATE_signed_char:
+            return arena.create<types::CharTy>();
+        default: return nullptr;
+        }
+    }
+
+    glu::types::TypeBase *
+    handleComposedTypes(llvm::DICompositeType const *diCompositeType)
+    {
+        using namespace glu::types;
+
+        auto &astArena = _context.getASTMemoryArena();
+        auto &typesArena = _context.getTypesMemoryArena();
+        switch (diCompositeType->getTag()) {
+        case llvm::dwarf::DW_TAG_class_type:
+        case llvm::dwarf::DW_TAG_structure_type: {
+            if (auto *structDecl = llvm::dyn_cast_if_present<ast::StructDecl>(
+                    _ctx._diTypeCache[diCompositeType]
+                )) {
+                return structDecl->getType();
+            }
+
+            std::vector<ast::FieldDecl *> fieldDecls;
+            if (auto elements = diCompositeType->getElements()) {
+                for (auto *elem : elements) {
+                    if (auto *derivedType
+                        = llvm::dyn_cast<llvm::DIDerivedType>(elem)) {
+                        if (derivedType->getTag() != llvm::dwarf::DW_TAG_member)
+                            continue;
+                        auto fieldName = derivedType->getName();
+                        auto fieldType = lift(derivedType->getBaseType());
+                        if (!fieldType) {
+                            return nullptr;
+                        }
+                        fieldDecls.push_back(astArena.create<ast::FieldDecl>(
+                            SourceLocation::invalid,
+                            copyString(fieldName, astArena.getAllocator()),
+                            fieldType, nullptr, nullptr, ast::Visibility::Public
+                        ));
                     }
-                    fieldDecls.push_back(astArena.create<ast::FieldDecl>(
-                        SourceLocation::invalid,
-                        copyString(fieldName, astArena.getAllocator()),
-                        fieldType, nullptr, nullptr, ast::Visibility::Public
-                    ));
                 }
             }
+            auto structDecl = astArena.create<ast::StructDecl>(
+                _context, SourceLocation::invalid, nullptr,
+                copyString(diCompositeType->getName(), astArena.getAllocator()),
+                fieldDecls, nullptr, ast::Visibility::Public
+            );
+            _ctx._diTypeCache[diCompositeType] = structDecl;
+            _ctx._decls.push_back(structDecl);
+            return structDecl->getType();
         }
-        auto structDecl = astArena.create<ast::StructDecl>(
-            _context, SourceLocation::invalid, nullptr,
-            copyString(diCompositeType->getName(), astArena.getAllocator()),
-            fieldDecls, nullptr, ast::Visibility::Public
-        );
-        _ctx._diTypeCache[diCompositeType] = structDecl;
-        _ctx._decls.push_back(structDecl);
-        return structDecl->getType();
+        case llvm::dwarf::DW_TAG_array_type: {
+            if (auto elements = diCompositeType->getElements()) {
+                if (elements->getNumOperands() != 2) {
+                    return nullptr;
+                }
+                auto *subrange = llvm::dyn_cast<llvm::DISubrange>(elements[1]);
+                if (!subrange) {
+                    return nullptr;
+                }
+                auto *baseType = lift(diCompositeType->getBaseType());
+                if (!baseType) {
+                    return nullptr;
+                }
+                auto count
+                    = llvm::isa<llvm::ConstantInt *>(subrange->getCount())
+                    ? llvm::cast<llvm::ConstantInt *>(subrange->getCount())
+                          ->getSExtValue()
+                    : -1;
+                if (count < 0) {
+                    return nullptr;
+                }
+                return typesArena.create<StaticArrayTy>(
+                    baseType, static_cast<size_t>(count)
+                );
+            }
+            return nullptr;
+        }
+        case llvm::dwarf::DW_TAG_enumeration_type: {
+            if (auto *enumDecl = llvm::dyn_cast_if_present<ast::EnumDecl>(
+                    _ctx._diTypeCache[diCompositeType]
+                )) {
+                return enumDecl->getType();
+            }
+
+            std::vector<ast::FieldDecl *> enumerators;
+            if (auto elements = diCompositeType->getElements()) {
+                for (auto *elem : elements) {
+                    if (auto *enumerator
+                        = llvm::dyn_cast<llvm::DIEnumerator>(elem)) {
+                        auto name = enumerator->getName();
+                        enumerators.push_back(astArena.create<ast::FieldDecl>(
+                            SourceLocation::invalid,
+                            copyString(name.str(), astArena.getAllocator()),
+                            nullptr
+                        ));
+                    }
+                }
+            }
+            auto enumDecl = astArena.create<ast::EnumDecl>(
+                _context, SourceLocation::invalid, nullptr,
+                copyString(
+                    diCompositeType->getName().str(), astArena.getAllocator()
+                ),
+                enumerators
+            );
+            _ctx._diTypeCache[diCompositeType] = enumDecl;
+            _ctx._decls.push_back(enumDecl);
+            return enumDecl->getType();
+        }
+        default: return nullptr;
+        }
     }
-    case llvm::dwarf::DW_TAG_array_type: {
-        if (auto elements = diCompositeType->getElements()) {
-            if (elements->getNumOperands() != 2) {
-                return nullptr;
-            }
-            auto *subrange = llvm::dyn_cast<llvm::DISubrange>(elements[1]);
-            if (!subrange) {
-                return nullptr;
-            }
-            auto *baseType = lift(diCompositeType->getBaseType());
+
+    glu::types::TypeBase *
+    handleDerivedType(llvm::DIDerivedType const *diDerivedType)
+    {
+        using namespace glu::types;
+
+        auto &typesArena = _context.getTypesMemoryArena();
+        auto tag = diDerivedType->getTag();
+
+        switch (tag) {
+        case llvm::dwarf::DW_TAG_pointer_type: {
+            auto *baseType = lift(diDerivedType->getBaseType());
             if (!baseType) {
                 return nullptr;
             }
-            auto count = subrange->getCount().dyn_cast<llvm::ConstantInt *>()
-                ? subrange->getCount()
-                      .dyn_cast<llvm::ConstantInt *>()
-                      ->getSExtValue()
-                : -1;
-            if (count < 0) {
+            return typesArena.create<PointerTy>(baseType);
+        }
+        case llvm::dwarf::DW_TAG_typedef:
+        case llvm::dwarf::DW_TAG_const_type:
+        case llvm::dwarf::DW_TAG_volatile_type:
+            return lift(diDerivedType->getBaseType());
+        default: return nullptr;
+        }
+    }
+
+    glu::types::TypeBase *
+    handleSubroutineType(llvm::DISubroutineType const *diSubroutineType)
+    {
+        using namespace glu::types;
+
+        auto &typesArena = _context.getTypesMemoryArena();
+        auto types = diSubroutineType->getTypeArray();
+
+        if (types.size() == 0) {
+            return nullptr;
+        }
+
+        // First element is return type
+        auto *returnType = lift(types[0]);
+        if (!returnType) {
+            return nullptr;
+        }
+
+        // Remaining elements are parameter types
+        llvm::SmallVector<TypeBase *, 4> paramTypes;
+        for (size_t i = 1; i < types.size(); ++i) {
+            auto *paramType = lift(types[i]);
+            if (!paramType) {
                 return nullptr;
             }
-            return typesArena.create<StaticArrayTy>(
-                baseType, static_cast<size_t>(count)
-            );
-        }
-        return nullptr;
-    }
-    case llvm::dwarf::DW_TAG_enumeration_type: {
-        if (auto *enumDecl = llvm::dyn_cast_if_present<ast::EnumDecl>(
-                _ctx._diTypeCache[diCompositeType]
-            )) {
-            return enumDecl->getType();
+            paramTypes.push_back(paramType);
         }
 
-        std::vector<ast::FieldDecl *> enumerators;
-        if (auto elements = diCompositeType->getElements()) {
-            for (auto *elem : elements) {
-                if (auto *enumerator
-                    = llvm::dyn_cast<llvm::DIEnumerator>(elem)) {
-                    auto name = enumerator->getName();
-                    enumerators.push_back(astArena.create<ast::FieldDecl>(
-                        SourceLocation::invalid,
-                        copyString(name.str(), astArena.getAllocator()), nullptr
-                    ));
-                }
-            }
-        }
-        auto enumDecl = astArena.create<ast::EnumDecl>(
-            _context, SourceLocation::invalid, nullptr,
-            copyString(
-                diCompositeType->getName().str(), astArena.getAllocator()
-            ),
-            enumerators
+        return FunctionTy::create(
+            typesArena.getAllocator(), paramTypes, returnType, false
         );
-        _ctx._diTypeCache[diCompositeType] = enumDecl;
-        _ctx._decls.push_back(enumDecl);
-        return enumDecl->getType();
     }
-    default: return nullptr;
-    }
-}
-
-glu::types::TypeBase *
-DITypeLifter::handleDerivedType(llvm::DIDerivedType const *diDerivedType)
-{
-    using namespace glu::types;
-
-    auto &typesArena = _context.getTypesMemoryArena();
-    auto tag = diDerivedType->getTag();
-
-    switch (tag) {
-    case llvm::dwarf::DW_TAG_pointer_type: {
-        auto *baseType = lift(diDerivedType->getBaseType());
-        if (!baseType) {
-            return nullptr;
-        }
-        return typesArena.create<PointerTy>(baseType);
-    }
-    case llvm::dwarf::DW_TAG_typedef:
-    case llvm::dwarf::DW_TAG_const_type:
-    case llvm::dwarf::DW_TAG_volatile_type:
-        return lift(diDerivedType->getBaseType());
-    default: return nullptr;
-    }
-}
-
-glu::types::TypeBase *DITypeLifter::handleSubroutineType(
-    llvm::DISubroutineType const *diSubroutineType
-)
-{
-    using namespace glu::types;
-
-    auto &typesArena = _context.getTypesMemoryArena();
-    auto types = diSubroutineType->getTypeArray();
-
-    if (types.size() == 0) {
-        return nullptr;
-    }
-
-    // First element is return type
-    auto *returnType = lift(types[0]);
-    if (!returnType) {
-        return nullptr;
-    }
-
-    // Remaining elements are parameter types
-    llvm::SmallVector<TypeBase *, 4> paramTypes;
-    for (size_t i = 1; i < types.size(); ++i) {
-        auto *paramType = lift(types[i]);
-        if (!paramType) {
-            return nullptr;
-        }
-        paramTypes.push_back(paramType);
-    }
-
-    return FunctionTy::create(
-        typesArena.getAllocator(), paramTypes, returnType, false
-    );
-}
-
-glu::types::TypeBase *DITypeLifter::lift(llvm::DIType const *diType)
-{
-    if (!diType) {
-        return _context.getTypesMemoryArena().create<types::VoidTy>();
-    }
-    switch (diType->getMetadataID()) {
-    case llvm::Metadata::MetadataKind::DIBasicTypeKind:
-        return handleBasicType(llvm::cast<llvm::DIBasicType>(diType));
-    case llvm::Metadata::MetadataKind::DICompositeTypeKind:
-        return handleComposedTypes(llvm::cast<llvm::DICompositeType>(diType));
-    case llvm::Metadata::MetadataKind::DIDerivedTypeKind:
-        return handleDerivedType(llvm::cast<llvm::DIDerivedType>(diType));
-    case llvm::Metadata::MetadataKind::DISubroutineTypeKind:
-        return handleSubroutineType(llvm::cast<llvm::DISubroutineType>(diType));
-    default: return nullptr;
-    }
-}
+};
 
 glu::types::TypeBase *
 lift(llvm::DIType const *diType, ModuleLiftingContext &ctx)
--- lib/IRDec/TypeLifter.cpp
+++ lib/IRDec/TypeLifter.cpp
@@ -12,88 +12,89 @@ class TypeLifter {
 
 public:
     TypeLifter(ModuleLiftingContext &ctx) : _ctx(ctx), _context(ctx.ast) { }
-    ~TypeLifter() = default;
 
     /// @brief Lift an LLVM type to a GLU type
     /// @param type The LLVM type to lift
     /// @return The lifted GLU type, or nullptr if the type could not be lifted
-    glu::types::TypeBase *lift(llvm::Type *type);
-};
+    glu::types::TypeBase *lift(llvm::Type *type)
+    {
+        auto &typesArena = _context.getTypesMemoryArena();
+        auto &astArena = _context.getASTMemoryArena();
 
-glu::types::TypeBase *TypeLifter::lift(llvm::Type *type)
-{
-    auto &typesArena = _context.getTypesMemoryArena();
-    auto &astArena = _context.getASTMemoryArena();
-
-    switch (type->getTypeID()) {
-    case llvm::Type::VoidTyID: return typesArena.create<types::VoidTy>();
-    case llvm::Type::HalfTyID: return typesArena.create<types::FloatTy>(16);
-    case llvm::Type::FloatTyID: return typesArena.create<types::FloatTy>(32);
-    case llvm::Type::DoubleTyID: return typesArena.create<types::FloatTy>(64);
-    case llvm::Type::X86_FP80TyID: return typesArena.create<types::FloatTy>(80);
-    case llvm::Type::FP128TyID: return typesArena.create<types::FloatTy>(128);
-    case llvm::Type::PPC_FP128TyID:
-        return typesArena.create<types::FloatTy>(128);
-    case llvm::Type::IntegerTyID: {
-        if (auto ty = llvm::dyn_cast<llvm::IntegerType>(type)) {
-            return typesArena.create<types::IntTy>(
-                types::IntTy::Signedness::Signed, ty->getIntegerBitWidth()
+        switch (type->getTypeID()) {
+        case llvm::Type::VoidTyID: return typesArena.create<types::VoidTy>();
+        case llvm::Type::HalfTyID: return typesArena.create<types::FloatTy>(16);
+        case llvm::Type::FloatTyID:
+            return typesArena.create<types::FloatTy>(32);
+        case llvm::Type::DoubleTyID:
+            return typesArena.create<types::FloatTy>(64);
+        case llvm::Type::X86_FP80TyID:
+            return typesArena.create<types::FloatTy>(80);
+        case llvm::Type::FP128TyID:
+            return typesArena.create<types::FloatTy>(128);
+        case llvm::Type::PPC_FP128TyID:
+            return typesArena.create<types::FloatTy>(128);
+        case llvm::Type::IntegerTyID: {
+            if (auto ty = llvm::dyn_cast<llvm::IntegerType>(type)) {
+                return typesArena.create<types::IntTy>(
+                    types::IntTy::Signedness::Signed, ty->getIntegerBitWidth()
+                );
+            }
+            return nullptr;
+        }
+        case llvm::Type::PointerTyID:
+            return typesArena.create<types::PointerTy>(
+                typesArena.create<types::CharTy>()
+            );
+        case llvm::Type::ArrayTyID: {
+            auto arrayTy = llvm::cast<llvm::ArrayType>(type);
+            return typesArena.create<types::StaticArrayTy>(
+                lift(arrayTy->getElementType()), arrayTy->getNumElements()
             );
         }
-        return nullptr;
-    }
-    case llvm::Type::PointerTyID:
-        return typesArena.create<types::PointerTy>(
-            typesArena.create<types::CharTy>()
-        );
-    case llvm::Type::ArrayTyID: {
-        auto arrayTy = llvm::cast<llvm::ArrayType>(type);
-        return typesArena.create<types::StaticArrayTy>(
-            lift(arrayTy->getElementType()), arrayTy->getNumElements()
-        );
-    }
-    case llvm::Type::StructTyID: {
-        auto structTy = llvm::dyn_cast<llvm::StructType>(type);
+        case llvm::Type::StructTyID: {
+            auto structTy = llvm::dyn_cast<llvm::StructType>(type);
 
-        if (auto *structDecl = llvm::dyn_cast_if_present<ast::StructDecl>(
-                _ctx._typeCache[structTy]
-            )) {
+            if (auto *structDecl = llvm::dyn_cast_if_present<ast::StructDecl>(
+                    _ctx._typeCache[structTy]
+                )) {
+                return structDecl->getType();
+            }
+
+            std::vector<ast::FieldDecl *> fieldDecls;
+            for (unsigned i = 0; i < structTy->getNumElements(); i++) {
+                llvm::Type *fieldTy = structTy->getElementType(i);
+                std::string name = ("F" + llvm::Twine(i)).str();
+                fieldDecls.push_back(astArena.create<ast::FieldDecl>(
+                    SourceLocation::invalid,
+                    copyString(name, astArena.getAllocator()), lift(fieldTy)
+                ));
+            }
+            auto structDecl = astArena.create<ast::StructDecl>(
+                _context, SourceLocation::invalid, nullptr,
+                copyString(
+                    structTy->getStructName().str(), astArena.getAllocator()
+                ),
+                fieldDecls
+            );
+            _ctx._typeCache[structTy] = structDecl;
+            _ctx._decls.push_back(structDecl);
             return structDecl->getType();
         }
-
-        std::vector<ast::FieldDecl *> fieldDecls;
-        for (unsigned i = 0; i < structTy->getNumElements(); i++) {
-            llvm::Type *fieldTy = structTy->getElementType(i);
-            std::string name = ("F" + llvm::Twine(i)).str();
-            fieldDecls.push_back(astArena.create<ast::FieldDecl>(
-                SourceLocation::invalid,
-                copyString(name, astArena.getAllocator()), lift(fieldTy)
-            ));
+        case llvm::Type::FunctionTyID: {
+            auto funcTy = llvm::cast<llvm::FunctionType>(type);
+            std::vector<types::TypeBase *> paramTypes;
+            for (unsigned i = 0; i < funcTy->getNumParams(); i++) {
+                paramTypes.push_back(lift(funcTy->getParamType(i)));
+            }
+            return typesArena.create<types::FunctionTy>(
+                paramTypes, lift(funcTy->getReturnType()), funcTy->isVarArg()
+            );
         }
-        auto structDecl = astArena.create<ast::StructDecl>(
-            _context, SourceLocation::invalid, nullptr,
-            copyString(
-                structTy->getStructName().str(), astArena.getAllocator()
-            ),
-            fieldDecls
-        );
-        _ctx._typeCache[structTy] = structDecl;
-        _ctx._decls.push_back(structDecl);
-        return structDecl->getType();
-    }
-    case llvm::Type::FunctionTyID: {
-        auto funcTy = llvm::cast<llvm::FunctionType>(type);
-        std::vector<types::TypeBase *> paramTypes;
-        for (unsigned i = 0; i < funcTy->getNumParams(); i++) {
-            paramTypes.push_back(lift(funcTy->getParamType(i)));
+        default: return nullptr;
         }
-        return typesArena.create<types::FunctionTy>(
-            paramTypes, lift(funcTy->getReturnType()), funcTy->isVarArg()
-        );
     }
-    default: return nullptr;
-    }
-}
+};
 
 glu::types::TypeBase *lift(llvm::Type *type, ModuleLiftingContext &ctx)
 {
